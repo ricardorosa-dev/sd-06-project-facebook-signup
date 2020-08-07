1ca1c60aef995237c2c81d881296218931a9ed9c
William Dieter <williamzdieter@gmail.com>
2020-08-07 18:12:45 -0300

    Requisitos 3-7

---
+++ script.js
@@ -0,0 +1,12 @@
+function buttonLogin() {
+	const botaoEntrar = document.getElementById('button-login');
+	const inputEmail = document.getElementById('user-email-phone');
+
+	botaoEntrar.addEventListener('click', function () {
+		window.alert(inputEmail.value);
+	});
+}
+
+window.onload = function () {
+	buttonLogin();
+}
