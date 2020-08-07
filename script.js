50a582eba67b6b6b69b9eb3adc724425feeb4d55
ricknois <rickssti@outlook.com>
2020-08-07 20:12:40 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btnLogin = document.querySelector('.button-login');
 
 
 btnLogin.addEventListener('click', function () {
-  if (document.getElementById('user-email-phone').value != ' ') {
+  if (document.getElementById('user-email-phone').value !== ' ') {
     alert(document.getElementById('user-email-phone').value);
   }
 });
