6532b49382049ccfea293bc99ce8d6a3e71ff7b8
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-08-07 17:09:13 -0300

    Corrigindo bugs do itens 3 ao 7

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const buttonEnter = document.querySelector('.button-login');
+const textEmailTel = document.getElementById('user-email-phone')
 
 function alertEmail() {
-  alert('Email ou telefone'); // falta terminar a função
+  alert(textEmailTel.value);
 }
 
 buttonEnter.addEventListener('click', alertEmail);
