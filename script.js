b3510f1826c9da14dbf3d5090652922853a3c9e7
Paulo Frazão <paulo4187@gmail.com>
2020-08-07 20:57:16 -0300

    correção code climate

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const buttonLogin = document.querySelector('#button-login');
 const inputUser = document.querySelector('#user-email-phone');
-const inputPassword = document.querySelector('#user-password');
 
 buttonLogin.addEventListener('click', function () {
   alert(inputUser.value);
