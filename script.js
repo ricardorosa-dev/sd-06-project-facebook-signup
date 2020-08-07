731b43d944d929344c099ef62d81a1071f43f06e
DehSartoreto <fixsartoreto@gmail.com>
2020-08-07 17:42:24 -0300

    codeclimate correction

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const mailEntry = document.querySelector('#user-email-phone');
 const btnSignup = document.querySelector('#facebook-register');
 const inputsValidate = document.querySelectorAll('.validate-inputs');
 const radioBtns = document.querySelectorAll('.radio-inputs');
+const customGender = false;
+const btnCustomGender = document.querySelector('#personalizado');
+
 
 btnLogin.addEventListener('click', function () {
   alert(mailEntry.value);
@@ -11,15 +14,17 @@ btnLogin.addEventListener('click', function () {
 btnSignup.addEventListener('click', function () {
   let temErro = 0;
   for (let i = 0; i < inputsValidate.length; i += 1) {
-    if (inputsValidate[i].value === "") {
+    if (inputsValidate[i].value === '') {
       temErro = 1;
-      break
+      break;
     }
   }
-  if (radioBtns[0].checked === false && radioBtns[1].checked === false && radioBtns[2].checked === false) {
+  if (radioBtns[0].checked === false && radioBtns[1].checked === false &&
+     radioBtns[2].checked === false) {
     temErro = 1;
   }
   if (temErro === 1) {
-    alert("Campos inválidos");
+    alert('Campos inválidos');
   }
-})
+});
+
