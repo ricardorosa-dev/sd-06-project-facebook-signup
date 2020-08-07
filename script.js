0a3b071b7d8217be9d6d39ed637f618d9209ce78
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-08-07 18:11:53 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ login.addEventListener('click', function () {
   const loginValue = document.querySelector('#user-email-phone').value;
   alert(loginValue);
 });
+function validFields () {
+  const inputs = document.querySelectorAll('.inputs-register');
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].checkValidity() === false) {
+      return false
+    }
+  }
+}
+const register = document.querySelector('#facebook-register');
+register.addEventListener('click', function () {
+  if (validFields() === false) {
+    const form = document.querySelector('.register');
+    const message = document.createElement('p');
+    message.innerHTML = 'Campos inválidos';
+    form.appendChild(message);
+  }
+});
