93ec518f7f0e43d676d6b66d3a73fe8280a9ccee
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-07 20:21:11 -0300

    Se crio o evento para quando dar click no botao entrar do login va mostrar un alert con el valor do campo email ou telefone

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  const buttonLogin = document.querySelector('#button-login');
+    const inputUser = document.querySelector('input#user-email-phone');
+    alert(inputUser);
+}
