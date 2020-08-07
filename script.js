e053a9720f9f5bc9846c74710f2096228b069c0a
Pedro Duarte <ikbenpedro@gmail.com>
2020-08-07 18:02:05 -0300

    adicionando bulma

---
+++ script.js
@@ -0,0 +1,5 @@
+let buttonLogin = document.getElementById('button-login');
+let inputEmailPhone = document.getElementById('user-email-phone');
+buttonLogin.addEventListener('click', function() {
+    alert(inputEmailPhone.value);
+});
