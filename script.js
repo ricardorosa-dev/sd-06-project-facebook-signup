2d7dac23288cb170ff926d8b350e808c5e2d08d3
kissyla <kissylamottapereira@gmail.com>
2020-08-07 19:54:26 -0300

    retirando erros codeclimite

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.getElementById('button-login').addEventListener('click', function () {
-  document.querySelector('user-email-phone').value;
-  console.log(document.querySelector('user-email-phone').value);
-  event.preventDefault();
-});
+//document.getElementById('button-login').addEventListener('click', function () {
+//document.querySelector('user-email-phone').value;
+//console.log(document.querySelector('user-email-phone').value);
+//event.preventDefault();
+//});
