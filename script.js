17d4fbc31a246d1619906e9357d0da23ef9f0b04
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-08-07 16:38:23 -0300

    Implementado o Requisito 7

---
+++ script.js
@@ -0,0 +1,9 @@
+function loginAlert() {
+  const email = document.querySelector('#user-email-phone').value;
+  alert(email);
+}
+
+window.onload = function() {
+  const loginBtn = document.querySelector('#button-login');
+  loginBtn.addEventListener('click', loginAlert);
+}
