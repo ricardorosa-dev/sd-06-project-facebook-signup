da46ddc41749f4d7e6a84cd4b2447ac436a351b9
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-07 19:40:31 -0300

    req. 18

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ function buttonLoginAlert() {
 }
 
 buttonLoginAlert();
+
+function invalidFormMessage() {
+  const form = document.querySelector('#register-form');
+  const warningMessage = document.createElement('div');
+  warningMessage.innerText = 'Campos inválidos';
+  warningMessage.classList.add('invalid-warning');
+  form.appendChild(warningMessage);
+}
+
+function validateForms() {
+  const arrayOfInputs = document.querySelectorAll('input');
+  let isValid = true;
+
+  for (let i = 0; i < arrayOfInputs.length; i += 1) {
+    if (arrayOfInputs[i].value === '') {
+      isValid = false;
+    }
+  }
+
+  if (!isValid) {
+    invalidFormMessage();
+  }
+}
+
+const registerButton = document.querySelector('#facebook-register');
+registerButton.addEventListener('click', validateForms);
