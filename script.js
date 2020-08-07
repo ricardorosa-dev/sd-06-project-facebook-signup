40add27616507cff15dfb19d2453f3dc1655efe2
DehSartoreto <fixsartoreto@gmail.com>
2020-08-07 16:20:00 -0300

    criando a top-bar

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnLogin = document.querySelector("#button-login");
+const mailEntry = document.querySelector("#user-email-phone");
+
+btnLogin.addEventListener("click", function() {
+    alert(mailEntry.value);
+})
