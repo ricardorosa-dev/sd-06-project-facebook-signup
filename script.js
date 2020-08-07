90a378f7d04bf17b0db9748899aa60c29f574972
luiz <phellipe501@gmail.com>
2020-08-07 18:13:04 -0300

    alinhando caixas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const entrar = document.querySelector('#button-login');
+  const text = document.querySelector('#user-email-phone');
   entrar.addEventListener('click', function () {
-    alert('Email ou telefone');
+    alert(text.value);
   });
 };
