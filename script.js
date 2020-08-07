2ada63aafcc764717d9893d119651598d61ad8b8
rafaelgelatti <rafael.gelatti@gmail.com>
2020-08-07 18:08:02 -0300

    teste form top bar

---
+++ script.js
@@ -0,0 +1,6 @@
+const login = document.getElementById('button-login');
+const nome = document.getElementById('user-email-phone');
+
+login.addEventListener('click', function () {
+  alert(nome.value);
+})
