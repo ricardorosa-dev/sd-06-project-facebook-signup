4f935f88c2c0844fc3716bdd995e7ab15e73b034
Micaeli Teixeira <luime6@gmail.com>
2020-08-07 20:48:33 -0300

    Colocando a parte do js para o botão entrar

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+
+ //FUNÇÃO PARA O BOTÃO ENTRAR
+ let warning = document.getElementById('button-login');
+ warning.addEventListener('click', function(){
+   const email = document.getElementById('user-email-phone').value;
+   email.innerHTML;
+   alert(email);
+ });
+ }
