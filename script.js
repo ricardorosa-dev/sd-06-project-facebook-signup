5bcc49c7ac4197251ba5137fb4ae3f90bba1f159
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-08-07 17:40:10 -0300

    Ajustar requisito 07 - remover espacamentos desnecessarios

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const buttonLogin = document.querySelector('#button-login');
-buttonLogin.addEventListener('click', function() {
-  alert('Email ou telefone');  
-})
+buttonLogin.addEventListener('click', function () {
+  alert('Email ou telefone');
+});
