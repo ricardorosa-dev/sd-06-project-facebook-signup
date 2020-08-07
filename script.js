2331f2beb3841738d69349158e21a86e0bbe654d
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-08-07 17:55:19 -0300

    8° requisito code climat

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let button = document.querySelector("#button-login");
+const button = document.querySelector('#button-login');
 
 button.addEventListener('click', function () {
-    let email = document.querySelector("#user-email-phone");
-    alert(email.value);
+  let email = document.querySelector('#user-email-phone');
+  alert(email.value);
 });
