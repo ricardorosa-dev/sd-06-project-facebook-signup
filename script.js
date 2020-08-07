fe15614101c1648eb35752c64db02e3be6123a1a
thalesgr <thalesgr@gmail.com>
2020-08-07 16:03:35 -0300

    correçao CC 1 ao 7

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonLogin = document.getElementById('button-login');
 
 buttonLogin.addEventListener('click', () => {
-  let emailPhoneValue = document.getElementById('email-phone').value;
-    alert(emailPhoneValue);
+  const emailPhoneValue = document.getElementById('email-phone').value;
+  alert(emailPhoneValue);
 });
