556a56b4c471e98e621077e243e9f0658030bc0f
ricknois <rickssti@outlook.com>
2020-08-07 20:16:14 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const btnLogin = document.querySelector('.button-login');
+// const btnLogin = document.querySelector('.button-login');
 
 
-btnLogin.addEventListener('click', function () {
-  if (document.getElementById('user-email-phone').value !== ' ') {
-    alert(document.getElementById('user-email-phone').value);
-  }
-});
+// btnLogin.addEventListener('click', function () {
+//   if (document.getElementById('user-email-phone').value !== ' ') {
+//     alert(document.getElementById('user-email-phone').value);
+//   }
+// });
