017cf73b1cacd4b5d32584aec117327ad609cd70
Paulo Frazão <paulo4187@gmail.com>
2020-08-07 20:55:38 -0300

    inclusão de propriedade na class top-bar

--- script.js
+++ script.js
@@ -3,19 +3,5 @@ const inputUser = document.querySelector('#user-email-phone');
 const inputPassword = document.querySelector('#user-password');
 
 buttonLogin.addEventListener('click', function () {
-  let login = true;
-  if (inputUser.value === '') {
-    alert('Campo Email ou Telefone precisa ser preenchido Por Favor');
-    login = false;
-  } else {
-    login = true;
-  }
-  if (inputPassword.value === '') {
-    alert('Campo senha precisa ser preechido');
-    login = false;
-  } else {
-    login = true;
-  } if (login === true) {
-    alert(`Login Efetuado Usuário:  ${inputUser.value}`);
-  }
+  alert(inputUser.value);
 });
