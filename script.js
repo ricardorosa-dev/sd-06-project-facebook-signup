ab5fda4bd4d88aeb0222b626390265e314334eaa
Vagner Pereira <sousa.vagner82@gmail.com>
2020-08-07 18:19:56 -0300

    adcionado requisito 18

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const emailTelephoneInput = document.querySelector('#user-email-phone');
 loginButton.addEventListener('click', function () {
   alert(emailTelephoneInput.value);
 });
+
+const facebookRegister = document.querySelector("#facebook-register");
+facebookRegister.addEventListener('click', function () {
+  const inputTextArray = document.querySelectorAll(".input-text-verification");
+  for (let index = 0; index < inputTextArray.length; index += 1) {
+    if (inputTextArray[index].value === '') {
+      alert("Campos inválidos");
+      break;
+    };
+  };
+});
