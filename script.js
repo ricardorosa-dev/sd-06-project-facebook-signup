d95f68920a9c2718237e9f66d26e9c639086d51a
Vagner Pereira <sousa.vagner82@gmail.com>
2020-08-07 19:02:55 -0300

    adcionado requisito 19

--- script.js
+++ script.js
@@ -5,13 +5,23 @@ loginButton.addEventListener('click', function () {
   alert(emailTelephoneInput.value);
 });
 
-const facebookRegister = document.querySelector("#facebook-register");
+const facebookRegister = document.querySelector('#facebook-register');
 facebookRegister.addEventListener('click', function () {
-  const inputTextArray = document.querySelectorAll(".input-text-verification");
+  const inputTextArray = document.querySelectorAll('.input-text-verification');
   for (let index = 0; index < inputTextArray.length; index += 1) {
     if (inputTextArray[index].value === '') {
-      alert("Campos inválidos");
+      alert('Campos inválidos');
       break;
-    };
-  };
+    }
+  }
+});
+
+const personalizeRadio = document.getElementById('personalizado');
+personalizeRadio.addEventListener('click', function () {
+  const input = document.createElement('input');
+  input.name = 'gender-custom';
+  input.placeholder = 'Gênero (opcional)';
+  const genderField = document.querySelector('.subscription-form-2');
+  const subscriptionForm = document.querySelector('.subscription-form');
+  subscriptionForm.insertBefore(input, facebookRegister);
 });
