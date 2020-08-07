183d6b9b02b73c72807b70f7fee2dc24db5aa0f5
Leandro Parisi <lepcbelisario@gmail.com>
2020-08-07 16:37:53 -0300

    ajustando code climate -2ª vez

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const loginButton = document.querySelector('#button-login');
 const emailTelephoneInput = document.querySelector('#user-email-phone');
 
 loginButton.addEventListener('click', function () {
-	alert(emailTelephoneInput.value);
+  alert(emailTelephoneInput.value);
 });
