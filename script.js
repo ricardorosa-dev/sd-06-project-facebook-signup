4ad508ca476d75811c4ff6da37ce0c2e499191d4
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-08-07 16:10:26 -0300

    Subindo itens 1 a 7 para teste

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttonEnter = document.querySelector('#button-login');
+
+function alertEmail () {
+ alert("Email ou telefone"); // falta terminar a função
+}
+
+buttonEnter.addEventListener('click', alertEmail);
