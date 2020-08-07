5efe31ea2617a7a2116f0f7f47bd79f9285e3aad
Daniel Jesus <dev.danieljs@gmail.com>
2020-08-07 17:53:41 -0300

    Adding function button event click with alert Email ou telefone

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+
+  const formButton = document.querySelector('#button-login');
+
+  formButton.addEventListener('click', function() {
+    alert('Email ou telefone');
+  });
+}
