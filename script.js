725a03342ce28a6a8c5fd5e13eb3bc8fe8defdd9
Danilo <dansantosalfa@gmail.com>
2020-08-07 19:58:19 -0300

    80% dos requisitos

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.querySelector('.user-email-phone').value);
 });
+
+const buttonSubmit = document.getElementsByClassName('form-input');
+buttonSubmit.addEventListener('click', function (){
+  for(index in buttonSubmit.length){
+    if(buttonSubmit === ''){
+      alert('')
+    }
+  }
+});
