f6af8483b3a4c7ecfb2f7dfe31454d986d3af333
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-08-07 16:24:14 -0300

    Requisito 10 valor email corrigido

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const login = document.querySelector('#button-login');
 login.addEventListener('click', function () {
-  alert('Email ou telefone');
+  let loginValue = document.querySelector('#user-email-phone').value;
+  alert(loginValue);
 });
