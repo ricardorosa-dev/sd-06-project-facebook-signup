a51042b4bfd7dfe358d61c24f11c7d51f61dd0f4
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-08-07 19:33:39 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -14,12 +14,32 @@ function validFields() {
   return result;
 }
 const register = document.querySelector('#facebook-register');
-register.addEventListener('click', function () {
+register.addEventListener('click', function (evt) {
   if (validFields() === false) {
     const form = document.querySelector('.register');
     const message = document.createElement('p');
     message.innerHTML = 'Campos inválidos';
     form.appendChild(message);
+  } else {
+    evt.preventDefault();
+    let firstname = document.querySelectorAll('.inputs-register')[0].value;
+    let lastname = document.querySelectorAll('.inputs-register')[1].value;
+    let contact = document.querySelectorAll('.inputs-register')[2].value;
+    let birthDate = document.querySelectorAll('.inputs-register')[4].value;
+    //let gender = document.querySelector('.gender:checked').value;
+    document.querySelector('.right-content').remove();
+    let contentRight = document.createElement('div');
+    contentRight.className = 'right-content';
+    let paragraph1 = document.createElement('p');
+    paragraph1.innerHTML = `Olá, ${firstname} ${lastname}`;
+    contentRight.appendChild(paragraph1);
+    let paragraph2 = document.createElement('p');
+    paragraph2.innerHTML = contact;
+    contentRight.appendChild(paragraph2);
+    let paragraph3 = document.createElement('p');
+    paragraph3.innerHTML = birthDate;
+    contentRight.appendChild(paragraph3);
+    document.querySelector('.main-content').appendChild(contentRight);
   }
 });
 const personalized = document.querySelector('#personalizado');
