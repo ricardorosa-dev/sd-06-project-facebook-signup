e9be2b172252adfa51431ce4f55236490472d4a3
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-08-07 16:09:19 -0300

    sétimo requisito

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleLoginButton() {
+  const loginBtn = document.getElementById('button-login');
+
+  loginBtn.onclick = () => {
+    alert("Email ou telefone");
+  }
+}
+
+window.onload = () => {
+  handleLoginButton();
+}
