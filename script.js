8a20000f234b3d624936e900aca902eb86ac48c1
MCoutinho <matheuscavalcante88@hotmail.com>
2020-08-07 19:26:29 -0300

    add gender custom input

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const loginButton = document.querySelector('#button-login');
 const emailInput = document.querySelector('#user-email-phone');
 const passwordInput = document.querySelector('#user-password');
 
+const customGenderInput = document.querySelector('#custom');
+
 loginButton.addEventListener('click', function () {
   if (emailInput.value && passwordInput.value) {
     alert(emailInput.value);
   }
+});
+
+customGenderInput.addEventListener('click', function () {
+  const customInput = document.createElement('input');
+  customInput.name = 'gender-custom';
+  customInput.placeholder = 'Gênero (opcional)';
+  document.querySelector('.input-gender-container').appendChild(customInput);
 });
