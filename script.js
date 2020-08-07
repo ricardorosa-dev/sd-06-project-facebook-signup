6cde1b3075611d95f3e72be5c7d4f236d8182530
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-07 17:14:16 -0300

    fixing seventh requirement

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function loginButton() {
-  alert(document.getElementById("user-email-phone").value);
+  alert(document.getElementById('user-email-phone').value);
 }
 
 window.onload = function () {
-  const button = document.getElementById("button-login");
-  button.addEventListener("click", loginButton);
-}
+  const button = document.getElementById('button-login');
+  button.addEventListener('click', loginButton);
+};
