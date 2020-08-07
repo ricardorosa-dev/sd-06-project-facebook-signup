048ce7cc4ab9cbc32d6b43df65bc3cd6ad96d82e
Alvaro Neto <alvaroshn@gmail.com>
2020-08-07 20:09:03 -0300

    ajustando espacos

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let buttonLogin = document.getElementById('button-login');
-let inputEmailPhone = document.getElementById('user-email-phone');
-let genderOptionOther = document.getElementById('other');
-let genderOptionFemale = document.getElementById('female');
-let genderOptionMale = document.getElementById('male');
-let genderFather = document.getElementById('gender-area');
+const buttonLogin = document.getElementById('button-login');
+const inputEmailPhone = document.getElementById('user-email-phone');
+const genderOptionOther = document.getElementById('other');
+const genderOptionFemale = document.getElementById('female');
+const genderOptionMale = document.getElementById('male');
+const genderFather = document.getElementById('gender-area');
 
-genderOptionFemale.addEventListener('click', function() {
-    genderFather.style.display = "none";
+genderOptionFemale.addEventListener('click', function () {
+  genderFather.style.display = 'none';
 });
-genderOptionMale.addEventListener('click', function() {
-    genderFather.style.display = "none";
+genderOptionMale.addEventListener('click', function () {
+  genderFather.style.display = 'none';
 });
-genderOptionOther.addEventListener('click', function() {
-    genderFather.style.display = "block";
+genderOptionOther.addEventListener('click', function () {
+  genderFather.style.display = 'block';
 });
-buttonLogin.addEventListener('click', function() {
-    alert(inputEmailPhone.value);
+buttonLogin.addEventListener('click', function () {
+  alert(inputEmailPhone.value);
 });
