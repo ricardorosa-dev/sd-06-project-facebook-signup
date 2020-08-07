4154cc1a13c18bd9d4464aa1bd5ea1d081b90439
Marthus Sera <marthus2001@gmail.com>
2020-08-07 16:22:04 -0300

    Code Climate

--- script.js
+++ script.js
@@ -1,41 +1,34 @@
 const buttonLogin = document.getElementById('button-login');
 const buttonSignUp = document.getElementById('facebook-register');
 const emailInput = document.getElementById('user-email-phone');
-const form = document.getElementById('formSignUp')
+const form = document.getElementById('formSignUp');
 
-buttonLogin.addEventListener('click', function() {
-  window.alert(emailInput.value);
-});
-
-buttonSignUp.addEventListener('click', function() {checkEmpty()});
-
-function checkEmpty(){
-  if(document.getElementById('firstname').value == '') {
-    invalidFields()
-    return
-  }
-  if(document.getElementById('lastname').value == '') {
-    invalidFields()
-    return
-  }
-  if(document.getElementById('phone_email').value == '') {
-    invalidFields()
-    return
-  }
-  if(document.getElementById('password').value == '') {
-    invalidFields()
-    return
-  }
-  if(document.getElementById('birthdate').value == '') {
-    invalidFields()
-    return
-  }
-
-  return
-}
-
-function invalidFields(){
-  let invalid = document.createElement('p');
+function invalidFields() {
+  const invalid = document.createElement('p');
   invalid.appendChild(document.createTextNode('Campos inválidos'));
   form.appendChild(invalid);
 }
+
+function checkEmpty() {
+  if (document.getElementById('firstname').value === '') {
+    invalidFields();
+  }
+  if (document.getElementById('lastname').value === '') {
+    invalidFields();
+  }
+  if (document.getElementById('phone_email').value === '') {
+    invalidFields();
+  }
+  if (document.getElementById('password').value === '') {
+    invalidFields();
+  }
+  if (document.getElementById('birthdate').value === '') {
+    invalidFields();
+  }
+}
+
+buttonLogin.addEventListener('click', function () {
+  window.alert(emailInput.value);
+});
+
+buttonSignUp.addEventListener('click', function () { checkEmpty()});
