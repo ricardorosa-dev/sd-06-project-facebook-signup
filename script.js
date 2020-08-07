146970bf628afe760762d46cc95c842ddcd084d7
rodrigoderkian <derkianpromo@gmail.com>
2020-08-07 17:53:10 -0300

    Adicionando alguns requisitos

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const buttonLogin = document.getElementById("button-login");
-const userEmailPhone = document.getElementById("user-email-phone")
+const userEmailPhone = document.getElementById("user-email-phone");
 
 function clickbutton() {
     alert(userEmailPhone.value);
 }
-
 
 window.onload = function () {
     buttonLogin.addEventListener('click', clickbutton);
