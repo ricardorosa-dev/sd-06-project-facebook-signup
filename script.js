badccfd6074720e69f223ba10e482b80f01bef9f
Marthus Sera <marthus2001@gmail.com>
2020-08-07 15:37:52 -0300

    Commit test

---
+++ script.js
@@ -0,0 +1,6 @@
+// const buttonLogin = document.getElementById('button-login');
+// const emailInput = document.getElementById('user-email-phone');
+
+// buttonLogin.addEventListener('click', function() {
+//   alert(email.value);
+// });
