7deee2945c3386a3b6b0c080d67365a6a7964a9b
kissyla <kissylamottapereira@gmail.com>
2020-08-07 19:23:16 -0300

    req  17 again

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('button-login').addEventListener('click', function () {
+  document.querySelector('user-email-phone').value;
+  console.log(document.querySelector('user-email-phone').value);
+  event.preventDefault();
+});
