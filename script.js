07f9e49e5e7c71d68f191e28232cb91b0fcc35fc
rodrigoderkian <derkianpromo@gmail.com>
2020-08-07 18:29:15 -0300

    Add requisitos 1 a 7

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-const buttonLogin = document.getElementById("button-login");
-const userEmailPhone = document.getElementById("user-email-phone");
-=======
 const buttonLogin = document.getElementById('button-login');
 const userEmailPhone = document.getElementById('user-email-phone');
->>>>>>> cbaf69adef8361d7f13940de9a0c544effeda755
 
 function clickbutton() {
   alert(userEmailPhone.value);
 }
 
+
 window.onload = function () {
-<<<<<<< HEAD
-    buttonLogin.addEventListener('click', clickbutton);
-=======
   buttonLogin.addEventListener('click', clickbutton);
->>>>>>> cbaf69adef8361d7f13940de9a0c544effeda755
 };
