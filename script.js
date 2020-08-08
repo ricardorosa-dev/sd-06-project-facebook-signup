2efa25e75a2d9e76549787fbdea34c5efe23ac51
Igor Leão <igorleao.eng@gmail.com>
2020-08-08 15:27:30 -0300

    personalisado e codeclimate

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const btn = document.querySelector('#button-login');
-const check = document.querySelector('.validation-check');
 
 btn.addEventListener('click', function () {
   const valor = document.getElementById('user-email-phone').value;
@@ -11,7 +10,6 @@ document.querySelector('#p').addEventListener('click', function () {
     const aux = document.createElement('input');
     aux.placeholder = 'Gênero (opcional)';
     aux.name = 'gender-custom';
-    aux.className = 'gender-custom-input';
-    document.querySelector('.radio-buttons').appendChild(newField);
+    document.querySelector('.radio-buttons').appendChild(aux);
   }
 });
