15b63ea9553481a5deb8472cb136e486a142e206
Julia Porto <juportob@gmail.com>
2020-08-08 11:10:22 -0300

    Add função

--- script.js
+++ script.js
@@ -4,3 +4,48 @@ btn.addEventListener('click', function () {
   const valor = document.getElementById('user-email-phone').value;
   alert(valor);
 });
+
+window.onload = function(){
+  new window.JustValidate('.forms', {
+    rules: {
+      firstname: {
+        required: true
+      },
+      lastname: {
+        required: true
+      },
+      phone_email: {
+        required: true
+      },
+      password: {
+        required: true
+      },
+      birthdate: {
+        required: true
+      },
+      genero: {
+        required: true
+      }
+    },
+    messages: {
+      firstname: {
+        required: "Campos inválidos"
+      },
+      lastname: {
+        required: "Campos inválidos"
+      },
+      phone_email: {
+        required: "Campos inválidos"
+      },
+      password: {
+        required: "Campos inválidos"
+      },
+      birthdate: {
+        required: "Campos inválidos"
+      },
+      genero: {
+        required: "Campos inválidos"
+      }
+    }
+  })
+}
