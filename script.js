f8d2c01a944f7aa18a6f81b71a69eeb836a7cefe
VinicinBraga <vinicius@bragamatta.com.br>
2020-08-08 10:37:09 -0300

    Corrigindo erro

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('#button-login');
+btn.addEventListener('click', function () {
+  const valor = document.getElementById('user-email-phone').value;
+  alert(valor);
+});
