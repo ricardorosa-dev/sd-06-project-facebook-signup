044f55bbcc9afe0adc10769c2f22eabd1caaf5a6
Havyner Caetano <hrcgeo@gmail.com>
2020-08-08 12:07:18 -0300

    Adicionar requisito 18 - validar campos vazios

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+const inputs = document.querySelectorAll('input');
 const buttonLogin = document.querySelector('#button-login');
-const inputEmailPhone = document.querySelector('#user-email-phone');
+const submitButton = document.querySelector('#facebook-register');
 
 buttonLogin.addEventListener('click', function () {
-  alert(inputEmailPhone.value);
+  alert(inputs[0].value);
+});
+
+submitButton.addEventListener('click', function (event) {
+  const errorMessage = document.querySelector('span');
+  let isEmpty = true;
+
+  for (let i = 2; i < inputs.length - 3; i += 1) {
+    if (inputs[0].checked || inputs[1].checked || inputs[2].checked) {
+      isEmpty = false;
+    }
+
+    if (inputs[i].value === '') {
+      errorMessage.innerText = 'Campos inválidos';
+      errorMessage.style.display = 'inline';
+      isEmpty = true;
+      break;
+    } else {
+      isEmpty = false;
+    }
+  }
+
+  if (isEmpty) {
+    event.preventDefault();
+  }
 });
