2d8eac5fd1f0667db53a7e7a661d9700e21dd966
LeandroGnu <leandro.feitoza@gmail.com>
2020-08-08 11:29:28 -0300

    23 - Refatorado a mensagem de erro para os campo requeridos e o value dos radio buttons

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const btnLogIn = document.querySelector('#button-login');
 btnLogIn.addEventListener('click', () => {
   alert(emailText.value);
 });
+
+const validateName = document.querySelectorAll('.sign-form input');
+for (let i = 0; i <= validateName.length; i += 1) {
+  validateName[i].oninvalid = function () {
+    this.setCustomValidity('');
+    if (!this.validity.valid) {
+      this.setCustomValidity('Campos inválidos');
+    }
+  };
+}
