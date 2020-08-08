042f548aed3a20af0d8e7b39134a6a00f12479d5
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-08-08 03:30:01 -0300

    Requisito 18: adicionando visibilidade dependente da caixa de genero personalizado

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ const functionalities = {
     const msg = document.querySelector('#user-email-phone').value;
     window.alert(msg);
   },
+  showCustomGenderBox: function customGenderBox() {
+    if (event.target.value === 'Personalizado') {
+      const optionalGenderElement = document.querySelector('#gender-opt');
+      optionalGenderElement.classList = 'selected';
+    } else {
+      const optionalGenderElement = document.querySelector('#gender-opt');
+      optionalGenderElement.classList = 'g-custom';
+    }
+  }
 };
 
 window.onload = function () {
   // Elementos
   const buttonLogin = document.querySelector('button');
+  const radiosButton = document.querySelectorAll('.radios input');
 
   // Eventos
   buttonLogin.addEventListener('click', functionalities.popUpEmailField);
+  radiosButton[0].addEventListener('click', functionalities.showCustomGenderBox);
+  radiosButton[1].addEventListener('click', functionalities.showCustomGenderBox);
+  radiosButton[2].addEventListener('click', functionalities.showCustomGenderBox);
 };
