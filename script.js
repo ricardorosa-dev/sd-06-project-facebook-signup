1628b7e61df5e8ff30a5b1693fdcc3162fc6347e
Micaeli Teixeira <luime6@gmail.com>
2020-08-07 21:23:56 -0300

    Acertando o alert

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
   // FUNÇÃO PARA O BOTÃO ENTRAR
   const warning = document.getElementById('button-login');
   warning.addEventListener('click', function () {
-   const email = document.getElementById('user-email-phone').value;
-    alert(email).innerHTML;
+    const email = document.getElementById('user-email-phone');
+    alert(email.value);
   });
 };
