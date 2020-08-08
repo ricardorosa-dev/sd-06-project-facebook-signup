e44218263cb59ad3f9b6192657c0d1165efb20e8
higoramorim <1810042@aluno.univesp.br>
2020-08-08 16:31:56 -0300

    js funções campos inválidos

--- script.js
+++ script.js
@@ -4,3 +4,48 @@ const inputEmailOrPhone = document.getElementById('user-email-phone');
 buttonEnter.addEventListener('click', () => {
   alert(inputEmailOrPhone.value);
 });
+
+const nameForm = document.getElementsByClassName('name');
+const lastnameForm = document.getElementsByClassName('lastname');
+const emailPhoneForm = document.getElementsByClassName('email-phone');
+const passwordForm = document.getElementsByClassName('senha');
+const birthForm = document.getElementsByClassName('birth');
+
+const log = document.getElementById('log');
+
+function logSubmit(event) {
+  let arrayDateForm = [nameForm, lastnameForm, emailPhoneForm, passwordForm, birthForm];
+    arrayDateForm.forEach(element => {
+      element.value = '';
+      log.innerHTML = 'Campos inválidos';
+      event.preventDefault();
+  });
+}
+
+const maleForm = document.getElementById('masculino');
+const femaleForm = document.getElementById('feminino');
+const otherForm = document.getElementById('personalizado');
+
+function logSubmitGender(event) {
+  let arrayGender = [maleForm, femaleForm, otherForm];
+  if (maleForm.value || femaleForm.value || otherForm.value === ''){
+    log.innerHTML = 'Campos inválidos';
+    event.preventDefault();
+  }
+}
+const getGender = document.querySelector('.Gender');
+const buttonForm = document.getElementById('facebook-register');
+function makeDivPersonal(event) {
+  if (otherForm.value != ''){
+    let inputPersonal = document.createElement('input');
+    inputPersonal.name = 'gender-custom';
+    inputPersonal.placeholder = 'Gênero (opcional)';
+    getGender.appendChild(inputPersonal);
+    event.preventDefault();
+  }
+}
+
+const form = document.getElementById('form');
+buttonForm.addEventListener ('click', logSubmit);
+buttonForm.addEventListener ('click', logSubmitGender);
+otherForm.addEventListener ('click', makeDivPersonal);
