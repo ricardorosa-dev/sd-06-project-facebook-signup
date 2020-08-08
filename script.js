63e35182ce39eaa1b7b2272c9c0cc40fa5724584
VinicinBraga <vinicius@bragamatta.com.br>
2020-08-08 11:00:14 -0300

    Corrigindo erro e melhorando o CSS novamente eretirando o JS

--- script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('#button-login');
-
-btn.addEventListener('click', function () {
-  const valor = document.getElementById('user-email-phone').value;
-  alert(valor);
-});
