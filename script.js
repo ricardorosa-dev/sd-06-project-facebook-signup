cd6b864f8138e45d6496eecd11af8cc9b824e466
ilanribeiro <ilanribeiro@gmail.com>
2020-08-07 23:01:59 -0300

    criada verificaçao de validade de preenchimento de todos os campos. Ajustes de id e for nos radio buttons. v.2

--- script.js
+++ script.js
@@ -8,17 +8,15 @@ login.addEventListener('click', function () {
   alert(name.value);
 });
 
-submitBtn.addEventListener('click', function() {
+submitBtn.addEventListener ('click', function() {
   let count = 0;
   for (let i = 2; i < (inputArray.length - 3); i += 1) {
-    let resultCheck = inputArray[i].checkValidity();
+    const resultCheck = inputArray[i].checkValidity();
     if (resultCheck === false) {
       count += 1;
     }
   }
-  // if (inputArray[7] !== ':checked' && inputArray[8] !== ':checked' && inputArray[9] !== ':checked') {
-  //   count += 1;
-  // }
+
   if (count !== 0) {
     alert('campo vazio');
   }
