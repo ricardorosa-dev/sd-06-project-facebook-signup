a86c9b5fdd4b13460fc19bba51444b278bf7f4bc
Pedro Pegurski <pegurski@gmail.com>
2020-08-07 22:27:10 -0300

    Item 18 - Done

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ function alertEmail() {
 }
 
 buttonEnter.addEventListener('click', alertEmail);
+
+const btRegister = document.getElementById('facebook-register');
+btRegister.addEventListener('click', function () {
+  const fnameField = document.getElementsByName('firstname')[0].value === '';
+  const lnameField = document.getElementsByName('lastname')[0].value === '';
+  const celField = document.getElementsByName('phone_email')[0].value === '';
+  const passField = document.getElementsByName('password')[0].value === '';
+  const birthdate = document.getElementsByName('birthdate')[0].value === '';
+  // const passField = document.getElementsByName('password')[0].value === '';
+  const list = [fnameField, lnameField, celField, passField, birthdate]
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i] === true) {
+      const errorMsg = document.createElement('p');
+      errorMsg.style.color = 'red';
+      errorMsg.innerHTML = 'Campos inválidos';
+      const form = document.getElementById('form_register');
+      form.appendChild(errorMsg);
+      break;
+    }
+  }
+});
