74a9c43548cd0c1dd3c712cd0e97337283e640d7
higoramorim <1810042@aluno.univesp.br>
2020-08-07 21:21:00 -0300

    ajustes js e css codeclimate

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const buttonEnter = document.getElementById('button-login');
 const inputEmailOrPhone = document.getElementById('user-email-phone');
 
 buttonEnter.addEventListener('click', () => {
-    alert(inputEmailOrPhone.value);
+  alert(inputEmailOrPhone.value);
 });
-
-
