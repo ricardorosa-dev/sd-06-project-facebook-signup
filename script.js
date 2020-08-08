eee5e6d5bd907df083aa0491e480b7497cc9186f
LeandroGnu <leandro.feitoza@gmail.com>
2020-08-08 11:41:08 -0300

    Confirmando erro no Avaliador automativo

--- script.js
+++ script.js
@@ -3,13 +3,3 @@ const btnLogIn = document.querySelector('#button-login');
 btnLogIn.addEventListener('click', () => {
   alert(emailText.value);
 });
-
-const validateName = document.querySelectorAll('.sign-form input');
-for (let i = 0; i <= validateName.length; i += 1) {
-  validateName[i].oninvalid = function () {
-    this.setCustomValidity('');
-    if (!this.validity.valid) {
-      this.setCustomValidity('Campos inválidos');
-    }
-  };
-}
