7ada42271857f34bb2147493df555a21db13faab
Pablo Santos <pablo.trybe@gmail.com>
2020-08-07 21:19:08 -0300

    Printando email quando clica no botao

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.getElementById('button-login').addEventListener('click', function () {
-  alert('Email ou telefone');
-});
+const btnLogin = document.getElementById('button-login');
+const txtEmail = document.getElementById('user-email-phone').value;
+function printEmail() {
+  alert(txtEmail);
+}
+btnLogin.addEventListener('click', printEmail);
