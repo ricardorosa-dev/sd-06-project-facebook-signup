aaa171f1a9f052436f16fe6a670020ea450741db
jederson <jmacedo.ti@gmail.com>
2020-08-08 16:00:37 -0300

    atualização do botão personalizado 4 tentativa

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ document.querySelector('#button-login').addEventListener('click', function () {
 });
 
 function personalizedGender() {
-  const newGenre = document.createElement('input');
-  newGenre.name = 'gender-custom';
-  newGenre.type = 'text';
-  newGenre.placeholder = 'Gênero (opcional)';
-  document.querySelector('.label-gender').parentNode.appendChild(newGenre);
+  if (!document.querySelector('#gender-custom')) {
+    const newGenre = document.createElement('input');
+    newGenre.name = 'gender-custom';
+    newGenre.id = 'gender-custom';
+    newGenre.type = 'text';
+    newGenre.placeholder = 'Gênero (opcional)';
+    document.querySelector('.label-gender').parentNode.appendChild(newGenre);
+  }
 }
 document.querySelector('.Personalizado').addEventListener('click', personalizedGender);
+
+function removeCustom() {
+  if (document.querySelector('#gender-custom')) {
+    document.querySelector('#gender-custom').parentNode.removeChild(document.querySelector('#gender-custom'));
+  }
+}
+
+document.querySelector('#Feminino').addEventListener('click', removeCustom);
+document.querySelector('#Masculino').addEventListener('click', removeCustom);
