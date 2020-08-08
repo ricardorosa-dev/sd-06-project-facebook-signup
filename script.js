bf41da21ecde56fcd100aa311b683c29da0f415f
Bruna Franciele <brunafranciele1902@gmail.com>
2020-08-08 14:37:47 -0300

    corrigindo type

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const todos = document.querySelector('.generos');
 perso.addEventListener('click', function () {
   const novoInput = document.createElement('input');
   novoInput.type = 'text';
-  novoInput.name = 'gender';
+  novoInput.name = 'gender-custom';
   novoInput.placeholder = 'Gênero (opcional)';
   todos.appendChild(novoInput);
 });
