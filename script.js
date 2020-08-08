a54189861b1c262f121dda3d09a70e6b83f22258
VinicinBraga <vinicius@bragamatta.com.br>
2020-08-08 10:17:41 -0300

    criando btn de Entrar

---
+++ script.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector('#button-login');
+
+btn.addEventListener('click', function () {
+  let valor = document.getElementById('user-email-phone').value;
+  alert(valor);
+});
