02b887c1925ac21371e78ecbc4ef157265d07218
Anderson R <misterregisslv@gmail.com>
2020-08-08 00:39:30 -0300

    adição do menu genero personalizado

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.querySelector('#button-login').addEventListener('click', function () {
   const inputEmail = document.querySelector('#user-email-phone').value;
   alert(inputEmail);
 });
+
+function personalizedGenre() {
+  const newGenre = document.createElement('input');
+  newGenre.type = 'text';
+  newGenre.name = 'gender-custom';
+  document.querySelector('.label-gender').appendChild(newGenre);
+}
+document.querySelector('.personalizado').addEventListener('click', personalizedGenre);
