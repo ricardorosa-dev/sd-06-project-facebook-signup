e84eed6dac648300953834a408d2d1f085136103
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-08-08 03:07:56 -0300

    correcao alert button-login

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function buttonLoginAlert() {
   alert(document.getElementById('user-email-phone').value);
 }
+const buttonLogin = document.querySelector('#button-login');
+buttonLogin.addEventListener('click', buttonLoginAlert);
