a7bfaf9514e56b93e3e1e29dc81931395ee38d5d
Anderson <andersoni.trybe@gmail.com>
2020-08-08 11:39:59 -0300

    Form error msg

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.getElementById('button-login').addEventListener('click', () => alert(document.getElementById('user-email-phone').value));
+
+function setNewErrorMsg(formField) {
+  formField.setCustomValidity('Campos inválidos');
+  formField.addEventListener('focus', () => {
+    formField.setCustomValidity('');
+  });
+}
+
+const formFields = document.querySelectorAll('.form-input');
+formFields.forEach(setNewErrorMsg);
