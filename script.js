bc25ae4ca9f1f3934e1763b09d357e123311249b
VinicinBraga <vinicius@bragamatta.com.br>
2020-08-08 10:24:25 -0300

    Resolvendo erros do CodeClimate

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let btn = document.querySelector('#button-login');
-
+const btn = document.querySelector('#button-login');
 btn.addEventListener('click', function () {
-  let valor = document.getElementById('user-email-phone').value;
+  const valor = document.getElementById('user-email-phone').value;
   alert(valor);
 });
