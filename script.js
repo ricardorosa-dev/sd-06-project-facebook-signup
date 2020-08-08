73906ebad8403f6e77a266b7e8c911a1d493e16c
Micaeli Teixeira <luime6@gmail.com>
2020-08-07 21:01:16 -0300

    Acertando o javaScript

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-window.onload = function(){
-
- //FUNÇÃO PARA O BOTÃO ENTRAR
- let warning = document.getElementById('button-login');
- warning.addEventListener('click', function(){
+window.onload = function () {
+  // FUNÇÃO PARA O BOTÃO ENTRAR
+  const warning = document.getElementById('button-login');
+  warning.addEventListener('click', function () {
    const email = document.getElementById('user-email-phone').value;
-   email.innerHTML;
-   alert(email);
- });
- }
+    alert(email).innerHTML;
+  });
+};
