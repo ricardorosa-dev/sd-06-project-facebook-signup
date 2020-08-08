feaae730f144c4e4e23ffc20a5371f9c67928f0f
ilanribeiro <ilanribeiro@gmail.com>
2020-08-07 22:58:56 -0300

    criada verificaçao de validade de preenchimento de todos os campos. Ajustes de id e for nos radio buttons.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const login = document.getElementById('button-login');
-const nome = document.getElementById('user-email-phone');
+const name = document.getElementById('user-email-phone');
+const submitBtn = document.getElementById('facebook-register');
+const inputArray = document.querySelectorAll('input');
+
 
 login.addEventListener('click', function () {
-  alert(nome.value);
+  alert(name.value);
+});
+
+submitBtn.addEventListener('click', function() {
+  let count = 0;
+  for (let i = 2; i < (inputArray.length - 3); i += 1) {
+    let resultCheck = inputArray[i].checkValidity();
+    if (resultCheck === false) {
+      count += 1;
+    }
+  }
+  // if (inputArray[7] !== ':checked' && inputArray[8] !== ':checked' && inputArray[9] !== ':checked') {
+  //   count += 1;
+  // }
+  if (count !== 0) {
+    alert('campo vazio');
+  }
 });
