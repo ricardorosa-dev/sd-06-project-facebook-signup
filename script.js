6448234c8bf1ea58b61b16da3e8b653acc5eab41
Rods <rods.leite27@gmail.com>
2020-08-07 23:23:36 -0300

    correcao Requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const btnEntrar = document.getElementById('button-login');
-const emailCel = document.querySelector('#user-email-phone-label');
-
-btnEntrar.addEventListener('click', function () {
+function entrarAlerta(){
+  const btnEntrar = document.getElementById('button-login');
+  const emailCel = document.querySelector('#user-email-phone');
   alert(emailCel.value);
-});
+}
