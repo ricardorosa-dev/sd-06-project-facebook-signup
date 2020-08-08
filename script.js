d1e545c93b0127410866058d88ead97f40eccff7
Isadora Koga <isadorakoga710@gmail.com>
2020-08-08 15:18:40 -0300

    10

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const buttonLogin = document.getElementById('button-login');
 buttonLogin.addEventListener('click', function () {
   const inputEmail = document.getElementById('user-email-phone').value;
   window.alert(inputEmail);
+  inputEmail.innerHTML = '';
 });
