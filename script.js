66a2fd59b12cdc38b3290c11bc796383a3165265
Renan Cunha <renan2610@gmail.com>
2020-08-08 18:38:06 -0300

    adicionando requisito 15 - data de nascimento

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const buttonLogin = document.querySelector('#button-login');
 const emailPhoneInput = document.querySelector('#user-email-phone');
 
+// window.onload = function () {
+//   let picker = new Pikaday({
+//     field: document.getElementById('birthdate'),
+//   });
+// };
+
 buttonLogin.addEventListener('click', function () {
   alert(emailPhoneInput.value);
 });
