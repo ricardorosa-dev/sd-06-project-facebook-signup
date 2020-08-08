e6cef2b7b34869cf001fedc4f3ab9d66f3369ab3
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-08-07 21:09:44 -0300

    19° requisito

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-const button = document.querySelector('#button-login');
+const buttonTop = document.querySelector('#button-login');
+const email = document.querySelector('#user-email-phone');
+const radioButtonPersonalizado = document.querySelector('#personalized');
+const radioButtonMasculino = document.querySelector('#male');
+const radioButtonFeminino = document.querySelector('#female');
+const containerRadioButtons = document.querySelector('.radio');
+const customGender = document.createElement('input');
 
-button.addEventListener('click', function () {
-  const email = document.querySelector('#user-email-phone');
+buttonTop.addEventListener('click', function () {  
   alert(email.value);
+});
+
+radioButtonPersonalizado.addEventListener('change', function () {
+    customGender.placeholder = "Gênero (opcional)";
+    customGender.name = "gender-custom";
+    customGender.id = "personalizado"
+    containerRadioButtons.appendChild(customGender);    
+});
+
+radioButtonMasculino.addEventListener('change', function () {
+    let personalized = document.querySelector('#personalizado');
+    if (personalized !== null) {
+        containerRadioButtons.removeChild(personalized);
+    }       
+});
+
+radioButtonFeminino.addEventListener('change', function () {
+    let personalized = document.querySelector('#personalizado');
+    if (personalized !== null) {
+        containerRadioButtons.removeChild(personalized);
+    }       
 });
