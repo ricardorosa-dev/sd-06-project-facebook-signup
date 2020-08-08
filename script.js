8e43e16e72512ac60821b6ebcc7c739677787b0f
kissyla <kissylamottapereira@gmail.com>
2020-08-08 18:40:24 -0300

    ajustando js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const botao = document.querySelector("#user-email-phone");
-function alertUser(){
-  if(botao.value == ""){
-    alert("Por favor, digite um Email ou Celular")
-  }else
-  alert(botao.value)
+const botao = document.querySelector('#user-email-phone');
+function alertUser() {
+  if (botao.value === '') {
+    alert('Por favor, digite um Email ou Celular');
+  } else {
+  alert(botao.value);
 }
+}
+alertUser();
