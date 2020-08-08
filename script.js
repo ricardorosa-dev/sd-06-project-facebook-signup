510f7cf8e1596e2640fc0345cbb08c4a9f50df9d
RenatoCostaDev <renatotrybe@gmail.com>
2020-08-08 13:38:59 -0300

    25- atualizando projeto

--- script.js
+++ script.js
@@ -3,3 +3,35 @@ const btnLogIn = document.querySelector('#button-login');
 btnLogIn.addEventListener('click', () => {
   alert(emailText.value);
 });
+
+// new JustValidate('.form-register', {
+// rules: {
+//   firstname: {
+//     required: true
+//   },
+//   lastname: {
+//     required: true
+//   },
+//   phone_email: {
+//     required: true
+//   },
+//   password: {
+//     required: true
+//   },
+//   birthdate: {
+//     required: true
+//   },
+//   genderRadio: {
+//     required: true
+//   },
+// },
+// messages: {
+//   name: {
+//     required : "Campos inválidos" 
+//   },
+//   email: 'My custom message about error (one error message for all rules)'
+// },
+// submitHandler: function (form, values, ajax) {
+  
+// },
+// });
