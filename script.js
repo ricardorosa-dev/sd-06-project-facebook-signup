4fbd50eb49ae4b3ba4c23c46f4e3cd709bbc5700
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-08 20:29:38 -0300

    Task 18 : Validando os campos do Formulário

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
+
+window.onload = function () {
+  new JustValidate('.facebook-form', {
+    rules: {
+      nome: {
+        required : true
+      },
+      sobrenome: {
+        required : true
+      },
+      phone: {
+        required: true
+      },
+      password: {
+        required : true
+      },
+      date: {
+        required : true
+      },
+      radio: {
+        required : true
+      },
+    },
+    messages: {
+      nome: {
+        required : 'Campos Inválidos'
+      },
+      sobrenome: {
+        required : 'Campos Inválidos'
+      },
+      phone: {
+        required : 'Campos Inválidos'
+      },
+      password: {
+        required : 'Campos Inválidos'
+      },
+      date: {
+        required : 'Campos Inválidos'
+      },
+      radio: {
+        required : 'Campos Inválidos'
+      },
+    },
+    submitHandler: function (form, values) {
+
+    },
+  });
+}
