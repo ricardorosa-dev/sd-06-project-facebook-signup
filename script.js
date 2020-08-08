9ce5b17466095919a31ccc992b86d2cdc2ec43ea
RenatoCostaDev <renatotrybe@gmail.com>
2020-08-08 14:55:42 -0300

    30- Inclusão de textarea para personalizar o gênero

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const btnLogIn = document.querySelector('#button-login');
 btnLogIn.addEventListener('click', () => {
   alert(emailText.value);
 });
+
+const btnRadioPersonal = document.querySelector('#personalizado');
+const spanPersonal = document.querySelector('#text-area-personalizado');
+// const btnRegister = document.querySelector('#facebook-register');
+btnRadioPersonal.addEventListener('click', () => {
+  event.preventDefault();
+  const txtArea = document.createElement('textarea');
+  txtArea.placeholder = 'Gênero (opcional)';
+  txtArea.name = 'gender-custom';
+  spanPersonal.appendChild(txtArea);
+});
