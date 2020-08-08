7c1da60e1062edfb7c85a34cf692ed13cce3b7b9
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-08 19:31:35 -0300

    Add gender custom

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const btnLogin = document.getElementById('button-login');
 const userEmailPhone = document.getElementById('user-email-phone');
-const invalidMessage = document.getElementById('invalid-input');
-const btnSingUp = document.getElementById('facebook-register');
+// const invalidMessage = document.getElementById('invalid-input');
+// const btnSingUp = document.getElementById('facebook-register');
+// const radioGender = document.querySelector('.gender');
+const customRadio = document.getElementById('custom');
+const genderCustom = document.querySelector('.custom-option');
 
 function clickButton() {
   alert(userEmailPhone.value);
 }
 
+function customOption() {
+  const createCustom = document.createElement('input');
+  createCustom.name = 'gender-custom';
+  createCustom.placeholder = 'Gênero (opcional)';
+  genderCustom.appendChild(createCustom);
+}
+
 window.onload = function () {
   btnLogin.addEventListener('click', clickButton);
+  customRadio.addEventListener('change', customOption);
 };
