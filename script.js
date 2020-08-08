03ac8bd5769e4cb05799778d74e0bb503c2de82a
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-08-08 02:31:20 -0300

    Adding new input if gender 'other' is checked

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.querySelector('.user-email-phone').value);
 });
+
+document.querySelector('#other').addEventListener('click', function () {
+  if (document.querySelector('.gender-container').lastElementChild.className === '') {
+    const newField = document.createElement('input');
+    newField.placeholder = 'Gênero (opcional)';
+    newField.name = 'gender-custom';
+    newField.className = 'gender-custom-input';
+    document.querySelector('.gender-container').appendChild(newField);
+  }
+});
