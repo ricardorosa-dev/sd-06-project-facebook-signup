72ad6401392e5b8b992244223999ffc376534bb6
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-08-08 17:51:19 -0300

    Corrigido erros apontados pelo codeclimate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnLogin = document.querySelector('#button-login');
 const inputBox = document.querySelector('#user-email-phone');
 
-btnLogin.addEventListener('click', function() {
-    alert(inputBox.value);
-})
+btnLogin.addEventListener('click', function () {
+  alert(inputBox.value);
+});
