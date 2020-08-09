d51c4b0fd196a9388dfba8593f22776c8af729c3
Vagner Pereira <sousa.vagner82@gmail.com>
2020-08-08 23:25:32 -0300

    Adcionando requisito 18

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const loginButton = document.querySelector('#button-login');
 const emailTelephoneInput = document.querySelector('#user-email-phone');
+const femaleGender = document.querySelector('#feminino');
+const maleGender = document.querySelector('#masculino');
+const customGender = document.querySelector('#personalizado');
 
 loginButton.addEventListener('click', function () {
   alert(emailTelephoneInput.value);
@@ -8,12 +11,16 @@ loginButton.addEventListener('click', function () {
 const facebookRegister = document.querySelector('#facebook-register');
 facebookRegister.addEventListener('click', function () {
   const inputTextArray = document.querySelectorAll('.input-text-verification');
-  for (let index = 0; index < inputTextArray.length; index += 1) {
-    if (inputTextArray[index].value === '') {
-      alert('Campos inválidos');
-      break;
+  if (! femaleGender.checked && ! maleGender.checked && ! customGender.checked) {
+    alert('Campos inválidos');
+  } else {
+    for (let index = 0; index < inputTextArray.length; index += 1) {
+      if (inputTextArray[index].value === '') {
+        alert('Campos inválidos');
+        break;
+      }
     }
-  }
+  }  
 });
 /*
 const personalizeRadio = document.getElementById('personalizado');
@@ -27,17 +34,17 @@ const input = document.createElement('input');
 
 const form2 = document.querySelectorAll('.subscription-form-2')[0];
 form2.addEventListener('click', function (event) {
+  const genderCustom = document.getElementsByName('gender-custom')[0];
   const subscriptionForm = document.querySelector('.subscription-form');
-  if (event.target.value === 'Personalizado') {
+  if (event.target.value === 'Personalizado' && genderCustom === undefined) {
     const input = document.createElement('input');
     input.name = 'gender-custom';
     input.placeholder = 'Gênero (opcional)';
     subscriptionForm.insertBefore(input, facebookRegister);
   } else
     if (event.target.value === 'Feminino' || event.target.value === 'Masculino') {
-      const aux = document.getElementsByName('gender-custom')[0];
-      if (aux !== undefined) {
-        subscriptionForm.removeChild(aux);
+      if (genderCustom !== undefined) {
+        subscriptionForm.removeChild(genderCustom);
       }
     }
 });
