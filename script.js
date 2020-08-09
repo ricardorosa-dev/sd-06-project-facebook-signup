8648639a8bc215d4509eacf5f40adb294338fb42
Sttefanie Maciel <macielsttefanie@gmail.com>
2020-08-09 19:33:31 -0300

    style em ordem alfabetica

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*document.getElementById('button-login').addEventListener('click', function () {
+/* document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
 */
@@ -8,17 +8,17 @@ const customRadio = document.getElementById('custom');
 const genderCustom = document.querySelector('.custom-option');
 
 function clickButton() {
-  alert(userEmailPhone.value);
+    alert(userEmailPhone.value);
 }
 
 function customOption() {
-  const createCustom = document.createElement('input');
-  createCustom.name = 'gender-custom';
-  createCustom.placeholder = 'Gênero (opcional)';
-  genderCustom.appendChild(createCustom);
+    const createCustom = document.createElement('input');
+    createCustom.name = 'gender-custom';
+    createCustom.placeholder = 'Gênero (opcional)';
+    genderCustom.appendChild(createCustom);
 }
 
 window.onload = function() {
-  btnLogin.addEventListener('click', clickButton);
-  customRadio.addEventListener('change', customOption);
+    btnLogin.addEventListener('click', clickButton);
+    customRadio.addEventListener('change', customOption);
 };
