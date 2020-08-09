962b53e48460941ca677c7188d8fefbf1becbe0c
Cássio Rubens <cassiorubensadv@gmail.com>
2020-08-08 23:59:11 -0300

    Aujuste codeclimate

--- script.js
+++ script.js
@@ -11,17 +11,8 @@ function nullOrEmpty(str) {
 }
 
 function validateForm() {
-  if (nullOrEmpty('formRegister')
-    && nullOrEmpty('name')
-    && nullOrEmpty('surname')
-    && nullOrEmpty('phone_email')
-    && nullOrEmpty('password')
-    && nullOrEmpty('birthdate')
-    && nullOrEmpty('gender')) {
-    alert('Campos inválidos');
-    return false;
-  }
-  return true;
+  alert('Campos inválidos');
+  return nullOrEmpty('formRegister') && nullOrEmpty('name') && nullOrEmpty('surname') && nullOrEmpty('phone_email') && nullOrEmpty('password') && nullOrEmpty('birthdate') && nullOrEmpty('gender');
 }
 
 const facebookRegister = document.getElementById('facebook-register');
