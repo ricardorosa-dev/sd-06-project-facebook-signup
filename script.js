f0cba0bf83cc4a9845378b7e6aae9c2019e2f4d9
Jean Machado <jeanvictor.arq@gmail.com>
2020-08-09 20:24:21 -0300

    alteracao date 2

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 const button = document.getElementById('button-login');
 const inputEmail = document.getElementById('user-email-phone');
-let picker = "";
 
 function buttonAlert() {
   alert(inputEmail.value);
 }
-
-picker = new Pikaday({
-  field: document.getElementById('datepicker'),
-  onSelect: function(date) {
-    console.log(date);
-  }
-});
 
 window.onload = function () {
   button.addEventListener('click', buttonAlert);
