75677e693101f98748cb3f9c7eef3933c548953c
Paulo <paulocheloni@gmail.com>
2020-08-09 14:15:02 -0300

    corrigindo erros de lint em script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 window.onload = () => {
   let justValidate = new JustValidate('.js-form', {
-      rules: {
-        name: {
-          required: true,
-        },
-        lastname: {
-          required: true,
-        },
-        contact: {
-          required: true,
-        },
-        password: {
-          required: true,
-        },
-        date: {
-          required: true,
-        },
-        gender: {
-          required: true,
-        },
+    rules: {
+      name: {
+        required: true,
       },
+      lastname: {
+          required: true,
+      },
+      contact: {
+          required: true,
+      },
+      password: {
+          required: true,
+      },
+      date: {
+        required: true,
+      },
+      gender: {
+        required: true,
+      },
+    },
   });
 };
