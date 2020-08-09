79e5ab834680bca60127be432c455d96086b4bb2
luiz <phellipe501@gmail.com>
2020-08-08 22:43:01 -0300

    arrumando code climate

--- script.js
+++ script.js
@@ -2,28 +2,24 @@ window.onload = function () {
   const entrar = document.querySelector('#button-login');
   const text = document.querySelector('#user-email-phone');
   const gender = document.querySelectorAll('.radio');
-
   for (let index = 0; index < gender.length; index += 1) {
     gender[index].addEventListener('change', function () {
-      const box = document.querySelector(".option-gender");
+      const box = document.querySelector('.option-gender');
       const other = document.createElement("input");
-      if (gender[2].checked == true) {
+      if (gender[2].checked === true) {
         other.name = 'gender-custom';
         other.className = 'other-gender';
         other.placeholder = 'Gênero (opcional)';
-        box.appendChild(other)
+        box.appendChild(other);
       } else {
-        box.removeChild(document.querySelector('.other-gender'))
-      };
+        box.removeChild(document.querySelector('.other-gender'));
+      }
     });
-  };
-
+  }
   entrar.addEventListener('click', function () {
     alert(text.value);
   });
-
   const cadast = document.querySelector('#facebook-register');
-
   cadast.addEventListener('click', function (event) {
     event.preventDefault();
     const name = document.querySelector('.firstname');
