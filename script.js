53e1f7203fd62b7b9b10a338cc837399f7ba470e
Igor Leão <igorleao.eng@gmail.com>
2020-08-09 10:15:56 -0300

    teste da validação persnonalizada

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('#button-login');
-
+const aux = '';
 btn.addEventListener('click', function () {
   const valor = document.getElementById('user-email-phone').value;
   alert(valor);
@@ -14,9 +14,24 @@ function validate() {
   const inputs = document.querySelectorAll('input');
   let auxBol = true;
   for (let i = 0; i < inputs.length; i += 1) {
-    if (inputs[i].value === '') auxBol = false;
+    if (inputs[i].value === aux) auxBol = false;
   }
   if (!auxBol) messageInvalid();
 }
 const botaoRegistro = document.querySelector('#facebook-register');
 botaoRegistro.addEventListener('click', validate);
+
+function personalGender () {
+  const personId =  document.querySelector('#p');
+  personId.addEventListener('click', function () {
+    if (document.querySelector('.teste-genero').lastElementChild.className === aux) {
+      const auxChild = document.createElement('input');
+      auxChild.placeholder = 'Gênero (opcional)';
+      auxChild.name = 'gender-custom';
+      auxChild.className = 'gender-custom-testeEvaluator';
+      document.querySelector('.teste-genero').appendChild(auxChild);
+    }
+  });
+  
+}
+
