a3471dfdd70f90ea46ba9df11e624d6343b7166c
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-08-08 21:24:06 -0300

    Adding new features with JS

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const submitButton = document.querySelector('.submit-button');
+const rightContent = document.querySelector('.right-content');
+
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.querySelector('.user-email-phone').value);
 });
@@ -10,4 +13,60 @@ document.querySelector('#other').addEventListener('click', function () {
     newField.className = 'gender-custom-input';
     document.querySelector('.gender-container').appendChild(newField);
   }
+});
+
+function createPersonH1(nameValue, lastNameValue) {
+  const createH1 = document.createElement('h1');
+  createH1.innerHTML = `Olá  ${nameValue} ${lastNameValue}`;
+  return createH1;
+}
+
+function createSmallTag(innerHtml) {
+  const createSmall = document.createElement('small');
+  createSmall.innerHTML = innerHtml;
+  createSmall.className = 'confirmation-form';
+  return createSmall;
+}
+
+function dateChecker(date) {
+  const dateDay = parseInt(date.slice(8, 10), 10);
+  const dateMonth = parseInt(date.slice(5, 7), 10);
+  const dateYear = parseInt(date.slice(0, 4), 10);
+  return `${dateDay} ${dateMonth} ${dateYear}`;
+}
+
+function genderFilter(genderMale, genderFemale, genderOther) {
+  let genderValue;
+  if (genderMale.checked) {
+    genderValue = genderMale.value;
+  } else if (genderFemale.checked) {
+    genderValue = genderFemale.value;
+  } else if (genderOther.checked) {
+    genderValue = genderOther.value;
+  }
+  return genderValue;
+}
+
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  const nameValue = document.querySelector('.first-name').value;
+  const lastNameValue = document.querySelector('.last-name').value;
+  const emailPhoneValue = document.querySelector('.phone-email').value;
+  let birthDateValue = document.querySelector('.birth-date').value;
+  const genderMale = document.querySelector('#male');
+  const genderFemale = document.querySelector('#female');
+  const genderOther = document.querySelector('#other');
+  birthDateValue = dateChecker(birthDateValue);
+
+  rightContent.innerHTML = '';
+
+  const userName = createPersonH1(nameValue, lastNameValue);
+  const userEmailPhone = createSmallTag(emailPhoneValue);
+  const birthConfirmation = createSmallTag(birthDateValue);
+  let genderValue = genderFilter(genderMale, genderFemale, genderOther);
+  genderValue = createSmallTag(genderValue);
+  rightContent.appendChild(userName);
+  rightContent.appendChild(userEmailPhone);
+  rightContent.appendChild(birthConfirmation);
+  rightContent.appendChild(genderValue);
 });
