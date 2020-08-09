c812802a435b245930550ccfe79e5993c52b80e1
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-08-09 16:21:24 -0300

    Input field: gender-custom

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-document.querySelector('#button-login').onclick = () => {
-  alert(document.querySelector('#user-email-phone').value);
+const METHOD = function (tagName) { return document.querySelector(tagName); };
+
+const GENDER = METHOD('.gender-options');
+
+METHOD('#button-login').onclick = () => {
+  alert(METHOD('#user-email-phone').value);
+};
+
+METHOD('#gender-pers').onchange = () => {
+  if (GENDER.children.length === 6) {
+    const PERS = document.createElement('input');
+    PERS.name = 'gender-custom';
+    PERS.placeholder = 'Gênero (opcional)';
+    PERS.type = 'text';
+    GENDER.appendChild(PERS);
+  }
 };
