e189eabdd2818da3aca42fa0d8930bc1de8c864a
luiz <phellipe501@gmail.com>
2020-08-08 23:29:12 -0300

    ultimo requisito

--- script.js
+++ script.js
@@ -17,11 +17,23 @@ window.onload = function () {
   });
   document.querySelector('#facebook-register').addEventListener('click', function (event) {
     event.preventDefault();
-    const invalid = document.querySelector('#invalid');
     if (document.querySelector('.firstname').value === '' || document.querySelector('.lastname').value === '' || document.querySelector('.phone_email').value === '' || document.querySelector('.password').value === '' || document.querySelector('.birthdate').value === '') {
-      invalid.innerText = 'Campos inválidos';
+      document.querySelector('#invalid').innerText = 'Campos inválidos';
     } else {
-      invalid.innerText = '';
+      document.querySelector('#invalid').innerText = '';
+      substituir();
     }
   });
 };
+
+function substituir () {
+  for (let index = 0; index < document.querySelectorAll('.radio').length; index += 1) {
+    if (document.querySelectorAll('.radio')[index].checked === true) {
+      var gend = document.querySelectorAll('.radio')[index].value
+    }
+  }
+  const right = document.querySelector(".right-content");
+  right.innerHTML = '<h1>Olá, ' + document.querySelector('.firstname').value+ ' '+ document.querySelector('.lastname').value +'</h1>' + ' <br> E-mail ou telefone: ' + document.querySelector('.phone_email').value + ' <br> Data de Nascimento: ' + document.querySelector('.birthdate').value + '<br> Gênero: ' + gend;
+
+  ;
+}
