fbb30cf2394b9c012fb71efb6faf7da9c681ccce
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-08-09 09:51:31 -0300

    Implementada funcionalidade de validação do formulário (Requisito 18)

--- script.js
+++ script.js
@@ -3,7 +3,36 @@ function loginAlert() {
   alert(email);
 }
 
+function showInvalidFieldsError() {
+  const errorSpan = document.getElementById('signUp-error');
+  errorSpan.innerHTML = 'Campos inválidos';
+}
+
+function hideInvalidFieldsError() {
+  const errorSpan = document.getElementById('signUp-error');
+  errorSpan.innerHTML = '';
+}
+
+function validateInputs() {
+  const formInputs = document.forms['signUp-form'].getElementsByTagName('input');
+  for (let index = 0; index < formInputs.length; index += 1) {
+    const currentInput = formInputs[index];
+    if (!currentInput.value) {
+      showInvalidFieldsError();
+      break;
+    } else {
+      hideInvalidFieldsError();
+    }
+  }
+}
+
 window.onload = function () {
   const loginBtn = document.querySelector('#button-login');
   loginBtn.addEventListener('click', loginAlert);
+
+  const signUpBtn = document.getElementById('facebook-register');
+  signUpBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    validateInputs();
+  });
 };
