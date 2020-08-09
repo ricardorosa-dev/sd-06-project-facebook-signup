e1b8e0fc087fccd02a83d7a3abe15685ae901f7f
Sttefanie Maciel <macielsttefanie@gmail.com>
2020-08-09 17:11:17 -0300

    Estilizando css e add js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-document.getElementById('button-login').addEventListener('click', function () {
+/*document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
+*/
+const btnLogin = document.getElementById('button-login');
+const userEmailPhone = document.getElementById('user-email-phone');
+const customRadio = document.getElementById('custom');
+const genderCustom = document.querySelector('.custom-option');
+
+function clickButton() {
+  alert(userEmailPhone.value);
+}
+
+function customOption() {
+  const createCustom = document.createElement('input');
+  createCustom.name = 'gender-custom';
+  createCustom.placeholder = 'Gênero (opcional)';
+  genderCustom.appendChild(createCustom);
+}
+
+window.onload = function() {
+  btnLogin.addEventListener('click', clickButton);
+  customRadio.addEventListener('change', customOption);
+};
