a96e5bb03f5a879345897f4dd3e735c3cb995512
Cássio Rubens <cassiorubensadv@gmail.com>
2020-08-09 00:17:48 -0300

    Validar campos botão cadastre-se

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ function nullOrEmpty(str) {
 }
 
 function validateForm() {
-  alert('Campos inválidos');
   return nullOrEmpty('formRegister') && nullOrEmpty('name') && nullOrEmpty('surname') && nullOrEmpty('phone_email') && nullOrEmpty('password') && nullOrEmpty('birthdate') && nullOrEmpty('gender');
 }
 
 const facebookRegister = document.getElementById('facebook-register');
-facebookRegister.addEventListener('click', validateForm);
+facebookRegister.addEventListener('click', function () {
+  if (facebookRegister === validateForm) {
+    return true;
+  }
+  alert('Campos inválidos');
+});
