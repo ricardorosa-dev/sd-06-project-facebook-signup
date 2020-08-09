583c86cbf0c4fda00d10aa6ea4a09406c5fa7806
Jean Machado <jeanvictor.arq@gmail.com>
2020-08-09 20:17:46 -0300

    alteracao date

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const button = document.getElementById('button-login');
 const inputEmail = document.getElementById('user-email-phone');
+let picker = "";
 
 function buttonAlert() {
   alert(inputEmail.value);
 }
 
+picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  onSelect: function(date) {
+    console.log(date);
+  }
+});
+
 window.onload = function () {
   button.addEventListener('click', buttonAlert);
-
-  //INICIALIZACAO PIKADAY
-  var picker = new Pikaday({
-    field: document.getElementById('datepicker'),
-    onSelect: function(date) {
-      console.log(date);
-    }
-  });
 };
