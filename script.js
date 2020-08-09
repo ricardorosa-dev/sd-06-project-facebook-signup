048d5d6d8297834cc3b64e0fbcd9b844d10b4783
luiz <phellipe501@gmail.com>
2020-08-08 22:40:12 -0300

    adding other gender

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 window.onload = function () {
   const entrar = document.querySelector('#button-login');
   const text = document.querySelector('#user-email-phone');
+  const gender = document.querySelectorAll('.radio');
+
+  for (let index = 0; index < gender.length; index += 1) {
+    gender[index].addEventListener('change', function () {
+      const box = document.querySelector(".option-gender");
+      const other = document.createElement("input");
+      if (gender[2].checked == true) {
+        other.name = 'gender-custom';
+        other.className = 'other-gender';
+        other.placeholder = 'Gênero (opcional)';
+        box.appendChild(other)
+      } else {
+        box.removeChild(document.querySelector('.other-gender'))
+      };
+    });
+  };
+
   entrar.addEventListener('click', function () {
     alert(text.value);
   });
+
   const cadast = document.querySelector('#facebook-register');
 
   cadast.addEventListener('click', function (event) {
