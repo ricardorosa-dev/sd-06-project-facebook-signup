702537ccc1a28d92c622a38dcd76845514e40c8c
rafaelgelatti <rafael.gelatti@gmail.com>
2020-08-09 00:08:54 -0300

    updating repeats

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const login = document.getElementById('button-login');
 const name = document.getElementById('user-email-phone');
-const submitBtn = document.getElementById('facebook-register');
-const inputArray = document.querySelectorAll('input');
-const registerForm = document.querySelector('.register-form');
-const genderContainer = document.getElementsByClassName('gender-container');
-const personalizedRadio = document.getElementById('personalizado');
 
 login.addEventListener('click', function () {
   alert(name.value);
 });
+
+//submit form
+const submitBtn = document.getElementById('facebook-register');
+const inputArray = document.querySelectorAll('input');
+const registerForm = document.querySelector('.register-form');
+let invalid = false;
 
 submitBtn.addEventListener('click', function () {
   let count = 0;
@@ -19,17 +20,44 @@ submitBtn.addEventListener('click', function () {
     }
   }
 
-  if (count !== 0) {
+  if (count !== 0 && !invalid) {
     const paragraph = document.createElement('p');
     paragraph.innerText = 'Campos inválidos';
     registerForm.appendChild(paragraph);
+    invalid = true;
   }
 });
 
+
+//radios
+let personalizedGender = false;
+const personalizedInput = document.createElement('input');
+const personalizedRadio = document.getElementById('personalizado');
+const maleRadio = document.getElementById('masculino');
+const femaleRadio = document.getElementById('feminino');
+const personalizedGenderContainer = document.getElementById('personalized-gender-flex');
+
 personalizedRadio.addEventListener('click', function () {
-  const personalizedInput = document.createElement('input');
-  personalizedInput.name = 'gender-custom';
-  personalizedInput.placeholder = 'Gênero (opcional)';
-  personalizedInput.className = 'gender-input';
-  genderContainer[0].appendChild(personalizedInput);
+  if (!personalizedGender) {
+    personalizedInput.style.display = 'flex';
+    personalizedInput.name = 'gender-custom';
+    personalizedInput.placeholder = 'Gênero (opcional)';
+    personalizedInput.className = 'gender-input';
+    personalizedGenderContainer.appendChild(personalizedInput);
+    personalizedGender = true;
+  }
+});
+
+maleRadio.addEventListener('click', function () {
+  if(personalizedGender) {
+    personalizedInput.style.display = 'none';
+    personalizedGender = false;
+  }
+});
+
+femaleRadio.addEventListener('click', function () {
+  if(personalizedGender) {
+    personalizedInput.style.display = 'none';
+    personalizedGender = false;
+  }
 });
