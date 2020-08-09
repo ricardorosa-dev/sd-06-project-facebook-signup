dba86d27f27484681ebec328d7c2b000e612c6a6
DekoPossas <andrepossaster@gmail.com>
2020-08-09 15:21:04 -0300

    i changed name of form button to form bar bottun

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.onload = function () {
-  const formButton = document.querySelector('#button-login');
-  formButton.addEventListener('click', function () {
+  const formBarButton = document.querySelector('#button-login');
+  formBarButton.addEventListener('click', function () {
     const inputEmailValue = document.querySelector('#user-email-phone');
     alert(inputEmailValue.value);
   });
+
+
 };
