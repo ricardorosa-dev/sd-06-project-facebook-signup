8be921aae301ef0b18072e277c2fdaa383ccd223
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-09 18:08:03 -0300

    Task 18 Fixes

--- script.js
+++ script.js
@@ -1,51 +1,38 @@
+let errors = 0;
+
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
 
-window.onload = function () {
-  new JustValidate('.facebook-form', {
-    rules: {
-      nome: {
-        required : true
-      },
-      sobrenome: {
-        required : true
-      },
-      phone: {
-        required: true
-      },
-      password: {
-        required : true
-      },
-      date: {
-        required : true
-      },
-      radio: {
-        required : true
-      },
-    },
-    messages: {
-      nome: {
-        required : 'Campos Inválidos'
-      },
-      sobrenome: {
-        required : 'Campos Inválidos'
-      },
-      phone: {
-        required : 'Campos Inválidos'
-      },
-      password: {
-        required : 'Campos Inválidos'
-      },
-      date: {
-        required : 'Campos Inválidos'
-      },
-      radio: {
-        required : 'Campos Inválidos'
-      },
-    },
-    submitHandler: function (form, values) {
-
-    },
-  });
+function radioCheck() {
+  if(document.forms[1].elements[5].checked) {
+    document.forms[1].elements[6].value = ' ';
+    document.forms[1].elements[7].value = ' ';
+  } else if (document.forms[1].elements[6].checked) {
+    document.forms[1].elements[5].value = ' ';
+    document.forms[1].elements[7].value = ' ';
+  } else {
+    document.forms[1].elements[5].value = ' ';
+    document.forms[1].elements[6].value = ' ';
+  }
 }
+
+function verifyErrors() {
+  errors = 0;
+  radioCheck();
+  for (let i = 0; i < document.forms[1].elements.length - 1; i += 1) {
+    if (document.forms[1].elements[i].value === '') {
+      errors += 1;
+    }
+  }
+}
+
+document.getElementById('facebook-register').addEventListener('click', function () {
+  event.preventDefault();
+  verifyErrors();
+  if (errors > 0) {
+    document.getElementById('invalidos').style.display = 'block';
+  } else {
+    document.getElementById('invalidos').style.display = 'none';
+  }
+});
