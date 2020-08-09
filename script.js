86942fd143ad0526a2cfc0c172e8f4d9ddb80673
Paulo <paulocheloni@gmail.com>
2020-08-09 00:45:43 -0300

    alterando layout e adicionando validaçao

---
+++ script.js
@@ -0,0 +1,43 @@
+new JustValidate('.js-form', {
+    rules: {
+      name: {
+        required: true
+      },
+      lastname: {
+        required:true
+      },
+      contact: {
+        required: true
+      },
+      password: {
+        required:true,
+        strength: {
+          default: true
+        }
+      },
+      date: {
+        required:true
+      },
+      gender: {
+        required:true
+      }
+    },
+    messages: {
+      name: 'Campos Invalidos',
+      lastname: 'Campos Invalidos'
+    },
+
+    submitHandler: function (form, values, ajax) {
+
+      ajax({
+        url: 'https://just-validate-api.herokuapp.com/submit',
+        method: 'POST',
+        data: values,
+        async: true,
+        callback: function(response)  {
+          console.log(response)
+        }
+      });
+    },
+  });
+
