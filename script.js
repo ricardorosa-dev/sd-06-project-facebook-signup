3de21ec040290d2bc621afa23439157b03943bea
Anderson <andersoni.trybe@gmail.com>
2020-08-10 16:16:40 -0300

    For, for input elements

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 document.getElementById('button-login').addEventListener('click', () => alert(document.getElementById('user-email-phone').value));
+
+let data = document.querySelectorAll('input');
+
+for (let i = 2; i < data.length; i += 1) {
+  console.log(data[i].value)
+}
