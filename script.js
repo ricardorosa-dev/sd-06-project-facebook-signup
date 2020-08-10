71c1cc06db19b5436ada93c64f2a560171f136c6
wfelipefarina <wfelipefarina@gmail.com>
2020-08-10 16:08:21 -0300

    update style.css codeclimate

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-let button = document.querySelector('#facebook-register')
-let radios = document.getElementsByName('gender');
-let form = document.querySelector('#formulary');
-let sectionRadios = document.querySelector('.gender');
-let inputs = document.querySelectorAll('#formulary input');
-let dataLogin = document.querySelectorAll('.facebook-login input')
-let login = document.querySelector('#button-login')
+// let button = document.querySelector('#facebook-register')
+// let radios = document.getElementsByName('gender');
+// let form = document.querySelector('#formulary');
+// let sectionRadios = document.querySelector('.gender');
+// let inputs = document.querySelectorAll('#formulary input');
+// let dataLogin = document.querySelectorAll('.facebook-login input')
+// let login = document.querySelector('#button-login')
 
 
-function alertLogin() {
-    alert(dataLogin[0].value)
-}
+// function alertLogin() {
+//     alert(dataLogin[0].value)
+// }
 
-login.addEventListener('click', alertLogin)
+// login.addEventListener('click', alertLogin)
 
-function validateData() {
-    event.preventDefault()
-    let p = document.createElement('p');
+// function validateData() {
+//     event.preventDefault()
+//     let p = document.createElement('p');
     
-    for(let i = 0; i < inputs.length; i +=1) {
-       if(inputs[i].value === '') {
-           p.innerHTML = 'Campos invalidos'
-           form.appendChild(p)
-       }
-    }
-    for(let i = 0; i < radios.length; i += 1) {
-        if(radios[i].checked) {
-            return radios[i].value
-        } else {
-            p.innerHTML = 'Campos invalidos'
-            form.appendChild(p)
-        }
-    }
-}
+//     for(let i = 0; i < inputs.length; i +=1) {
+//        if(inputs[i].value === '') {
+//            p.innerHTML = 'Campos invalidos'
+//            form.appendChild(p)
+//        }
+//     }
+//     for(let i = 0; i < radios.length; i += 1) {
+//         if(radios[i].checked) {
+//             return radios[i].value
+//         } else {
+//             p.innerHTML = 'Campos invalidos'
+//             form.appendChild(p)
+//         }
+//     }
+// }
 
-button.addEventListener('click', validateData);
+// button.addEventListener('click', validateData);
 
-function addCampo() {
-    let inputGender = document.createElement('input');
-    inputGender.setAttribute('placeholder', 'Genero(opcional)')
+// function addCampo() {
+//     let inputGender = document.createElement('input');
+//     inputGender.setAttribute('placeholder', 'Genero(opcional)')
 
-    for(let i = 0; i < radios.length; i += 1) {
-        radios[i].addEventListener('click', function() {
-            if(radios[i].value == 'Personalizado') {                
-                form.insertBefore(inputGender, button);        
-            }
-        });
-    }
-}
+//     for(let i = 0; i < radios.length; i += 1) {
+//         radios[i].addEventListener('click', function() {
+//             if(radios[i].value == 'Personalizado') {                
+//                 form.insertBefore(inputGender, button);        
+//             }
+//         });
+//     }
+// }
 
-addCampo();
+// addCampo();
 
 
