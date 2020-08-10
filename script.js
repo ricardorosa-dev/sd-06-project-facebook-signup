e40bd79eb267bdc4d565b8f5ae8a4e328ecf14e3
DekoPossas <andrepossaster@gmail.com>
2020-08-09 21:41:32 -0300

    corrected the code climate

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ const newUserButton = document.querySelector('#facebook-register');
 const camps = document.querySelectorAll('.required');
 newUserButton.addEventListener('click', function () {
   for (let i = 0; i < camps.length; i += 1) {
-    if (camps[i].value === "") {
+    if (camps[i].value === '') {
       alert('Campos inválidos');
-      break
+      break;
     }
   }
-})
+});
