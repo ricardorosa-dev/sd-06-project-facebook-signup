f3dc059c9b1c55686b14ae4f641d7f53d1a1479a
vbergadev <vbergadev@gmail.com>
2020-08-10 11:12:47 -0300

    alinhamento cabeçalho

---
+++ script.js
@@ -0,0 +1,6 @@
+let entrar = document.getElementById('button-login')
+function entrada() {
+    let emailTelefone = document.getElementById('user-email-phone').value;
+    alert(emailTelefone);
+}
+entrar.addEventListener('click',entrada)
