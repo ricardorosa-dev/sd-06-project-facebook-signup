60d05114a64df5635444e1d1f482fd474444e5cb
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-08-10 14:49:18 -0300

    commit de requisitos 19

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ buttonLogin.addEventListener('click', () => {
   const emailPhoneValue = document.getElementById('user-email-phone').value;
   alert(emailPhoneValue);
 });
+document.getElementById('facebook-register').addEventListener('click', function() {
+  let nome = document.getElementById('firstname').value;
+  let sobreNome = document.getElementById('lastname').value;
+  let celularEmail = document.getElementById('phone_email').value;
+  let senha = document.getElementById('password').value;
+  let nascimento = document.getElementById('birthdate').value;
+  let genero = document.getElementById('feminino').value;
+  if ((nome === '') || (sobreNome === '') || (celularEmail === '') || (senha === '') || (nascimento === '') || (genero === '')) {
+    alert('Campos inválidos');
+  }
+});
