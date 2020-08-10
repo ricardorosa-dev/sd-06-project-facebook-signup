3da65a46b273d32482f71ad7fe5e8fb69180fe5a
higoramorim <1810042@aluno.univesp.br>
2020-08-10 12:52:31 -0300

    ajustes coding

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-const buttonEnter = document.getElementById('button-login');
-const inputEmailOrPhone = document.getElementById('user-email-phone');
+const METHOD = function (tagName) { return document.querySelector(tagName); };
 
-buttonEnter.addEventListener('click', () => {
-  alert(inputEmailOrPhone.value);
-});
+const GENDER = METHOD('.gender-container');
+const INPUT = document.querySelectorAll('input');
+const BTNREGISTER = METHOD('#facebook-register');
+let count = 0;
 
-const nameForm = document.getElementsByClassName('name');
-const lastnameForm = document.getElementsByClassName('lastname');
-const emailPhoneForm = document.getElementsByClassName('email-phone');
-const passwordForm = document.getElementsByClassName('senha');
-const birthForm = document.getElementsByClassName('birth');
+METHOD('#button-login').onclick = () => {
+  alert(METHOD('#user-email-phone').value);
+};
 
-const log = document.getElementById('log');
+METHOD('#gender-pers').onchange = () => {
+  if (METHOD('.gender-options').children.length === 6) {
+    const PERS = document.createElement('input');
+    PERS.name = 'gender-custom';
+    PERS.placeholder = 'Gênero (opcional)';
+    PERS.type = 'text';
+    GENDER.appendChild(PERS);
+  }
+};
 
-function logSubmit(event) {
-  const arrayDateForm = [nameForm, lastnameForm, emailPhoneForm, passwordForm, birthForm];
-  arrayDateForm.forEach((element) => {
-    element.value = '';
-    log.innerHTML = 'Campos inválidos';
-    event.preventDefault();
+function addErrorMsg(check) {
+  if (check) {
+    const TEXT = document.createElement('p');
+    count += 1;
+
+    if (count <= 1) {
+      TEXT.innerHTML = 'Campos inválidos';
+      GENDER.appendChild(TEXT);
+    }
+  }
+}
+
+BTNREGISTER.onclick = () => {
+  let check = false;
+
+  INPUT.forEach((item) => {
+    if (item.id !== 'user-email-phone' && item.id !== 'user-password') {
+      if (item.value === '') {
+        check = true;
+      }
+    }
   });
-}
 
-const maleForm = document.getElementById('masculino');
-const femaleForm = document.getElementById('feminino');
-const otherForm = document.getElementById('personalizado');
-
-function logSubmitGender(event) {
-  if (maleForm.value || femaleForm.value || otherForm.value === '') {
-    log.innerHTML = 'Campos inválidos';
-    event.preventDefault();
-  }
-}
-const getGender = document.querySelector('.Gender');
-const buttonForm = document.getElementById('facebook-register');
-function makeDivPersonal(event) {
-  if (otherForm.value !== '') {
-    const inputPersonal = document.createElement('input');
-    inputPersonal.name = 'gender-custom';
-    inputPersonal.placeholder = 'Gênero (opcional)';
-    getGender.appendChild(inputPersonal);
-    event.preventDefault();
-  }
-}
-
-buttonForm.addEventListener('click', logSubmit);
-buttonForm.addEventListener('click', logSubmitGender);
-otherForm.addEventListener('click', makeDivPersonal);
+  addErrorMsg(check);
+};
