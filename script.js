221530e506985dd2a625601cca9ff58e75baee76
DekoPossas <andrepossaster@gmail.com>
2020-08-09 21:38:39 -0300

    create new function, that validates input value.

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ window.onload = function () {
     alert(inputEmailValue.value);
   });
 };
+
+const newUserButton = document.querySelector('#facebook-register');
+const camps = document.querySelectorAll('.required');
+newUserButton.addEventListener('click', function () {
+  for (let i = 0; i < camps.length; i += 1) {
+    if (camps[i].value === "") {
+      alert('Campos inválidos');
+      break
+    }
+  }
+})
