01ab274bc614855494f85769bea5e7933f818e62
Dilenio Enderle <dilenio@gmail.com>
2020-08-10 13:22:47 -0300

    Correções JS no item 18

--- script.js
+++ script.js
@@ -3,22 +3,30 @@ buttonLogin.addEventListener('click', function () {
   alert(document.querySelector('#user-email-phone').value);
 });
 
-let fields = [];
-
-fields.push(document.querySelector('#firstname').value);
-fields.push(document.querySelector('#lastname').value);
-fields.push(document.querySelector('#phoneemail').value);
-fields.push(document.querySelector('#password').value);
-fields.push(document.querySelector('#birthdate').value);
-fields.push(document.querySelector('#gender').value);
+const fields = [];
 
 const facebookRegister = document.querySelector('#facebook-register');
 facebookRegister.addEventListener('click', function (event) {
   event.preventDefault();
-  console.log(event)
+  fields.push(document.querySelector('#firstname').value);
+  fields.push(document.querySelector('#lastname').value);
+  fields.push(document.querySelector('#phoneemail').value);
+  fields.push(document.querySelector('#password').value);
+  fields.push(document.querySelector('#birthdate').value);
+  if (document.querySelector('#feminino').checked === true) {
+    fields.push(document.querySelector('#feminino').value);
+  } else if (document.querySelector('#masculino').checked === true) {
+    fields.push(document.querySelector('#masculino').value);
+  } else if (document.querySelector('#personalizado').checked === true) {
+    fields.push(document.querySelector('#personalizado').value);
+  } else {
+    fields.push('');
+  }
   const result = document.querySelector('#result');
+  console.log(fields);
   for (let i = 0; i < fields.length; i += 1) {
     if (fields[i] === '') {
+      console.log(fields[i]);
       result.innerHTML = 'Campos inválidos';
     }
   }
