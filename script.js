ceee62c6c5b97f6148cacd94b8c45469470c4d34
Anderson <andersoni.trybe@gmail.com>
2020-08-10 17:14:02 -0300

    Select for gender (radio buttons function)

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 document.getElementById('button-login').addEventListener('click', () => alert(document.getElementById('user-email-phone').value));
 
 let data = document.querySelectorAll('input');
+let isEmpty = 0;
+const errorSpan = document.querySelector('#error-span');
+const selectedGenderSpan = document.querySelector('#selected-gender');
 
-for (let i = 2; i < data.length; i += 1) {
-  console.log(data[i].value)
+for (let i = 7; i < 9; i += 1) {
+  data[i].addEventListener('click', () => {
+    selectedGenderSpan.value = document.querySelector('input:checked').value;
+  })
 }
+
+// Verifica se o campo é vazio
+function checkContent() {
+  for (let i = 2; i < 7; i += 1) {
+    if (data[i].value === '') {
+      isEmpty += 1;
+    } else if (selectedGenderSpan.value = '') {
+      isEmpty += 1;
+    }
+  };
+  return isEmpty;
+}
+
+// Se tiver algum vazio, ele coloca o erro no span
+function isAnyFieldEmpty() {
+  if (checkContent()) {
+    errorSpan.innerText = 'Campos inválidos';
+    data.forEach(element => {
+      element.addEventListener('click', () => {
+        errorSpan.innerText = '';
+      })
+    });
+  };
+}
+
+document.querySelector('#facebook-register').addEventListener('click', () => {
+  event.preventDefault();
+  isAnyFieldEmpty();
+  console.log('click register')
+})
