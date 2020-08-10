72677e76324b1e22d9ca6babbf1be2381de03c2a
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-10 12:51:56 -0300

    20. Consolidado com as informações

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ const allGenders = document.querySelector('#genders');
 const inputs = [];
 const optional = document.querySelector('#optionalGender');
 
+// Alert do top-bar
 function alerta() {
   alert(emailLogin.value);
 }
 
+// Armazenando todos os inputs em um array
 function checkInputs() {
   inputs[0] = name.value;
   inputs[1] = lastName.value;
@@ -26,17 +28,41 @@ function checkInputs() {
   return inputs;
 }
 
-function cadastrar(event) {
-  event.preventDefault();
-  checkInputs();
-
-  for (let index = 0; index < inputs.length; index += 1) {
-    if (inputs[index] === '') {
-      message.innerHTML = 'Campos inválidos';
-    }
+// Deletando todos os campos
+function clearFields() {
+  while (form.firstChild) {
+    form.removeChild(form.firstChild);
   }
 }
 
+// Gerando um consolidado com as informações preenchidas
+function consolidate() {
+  const consolidateDiv = document.createElement('div');
+  consolidateDiv.innerHTML = `Olá, ${inputs[0]} ${inputs[1]}.<br>Seu e-mail ou telefone: ${inputs[2]}.<br>Data de Nascimento: ${inputs[4]}.<br>Genero: ${inputs[5]}.`;
+  consolidateDiv.className = 'consolidate';
+  form.appendChild(consolidateDiv);
+}
+
+// Função para o botão de cadastrar
+function cadastrar(event) {
+  event.preventDefault();
+  checkInputs();
+  let bool = true;
+  for (let index = 0; index < inputs.length; index += 1) {
+    if (inputs[index] === '') {
+      bool = false;
+    }
+  }
+
+  if (bool === false) {
+    message.innerHTML = 'Campos inválidos.';
+  } else {
+    clearFields();
+    consolidate(inputs);
+  }
+}
+
+// Criando gênero opcional
 function createGender() {
   const nameGender = document.createElement('input');
   nameGender.name = 'gender-custom';
@@ -45,6 +71,7 @@ function createGender() {
   optional.appendChild(nameGender);
 }
 
+// Condição se o usuário apertar em Personalizado
 function personalize(event) {
   const click = event.target;
   if (click.value === 'Personalizado') {
