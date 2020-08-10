02d38e744a4a522f42b341ff4c357b9273516d36
LeandroGnu <leandro.feitoza@gmail.com>
2020-08-10 00:46:28 -0300

    35- Corrigido a função para ao inves de alert. Mostrar mensagem dentro do Form

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let error = 0;
+const inputsForm = document.querySelectorAll('.validaForm');
+const messageParagraph = document.createElement('p');
+messageParagraph.innerText = 'Campos inválidos';
+
 const emailText = document.querySelector('#user-email-phone');
 const btnLogIn = document.querySelector('#button-login');
 btnLogIn.addEventListener('click', () => {
@@ -8,19 +13,27 @@ function testRadioChecked() {
   if (document.querySelector('#feminino').checked === false &&
     document.querySelector('#masculino').checked === false &&
     document.querySelector('#personalizado').checked === false) {
-    alert('Campos inválidos');
+    document.querySelector('#personal-field').appendChild(messageParagraph);
   }
 }
 
-const inputsForm = document.querySelectorAll('.validaForm');
-document.querySelector('#facebook-register').addEventListener('click', () => {
-  testRadioChecked();
+function inputValidate() {
   for (let i = 0; i <= inputsForm.length - 1; i += 1) {
     if (inputsForm[i].value === '') {
-      alert('Campos Inválidos');
+      error += 1;
     }
   }
+}
+
+document.querySelector('#facebook-register').addEventListener('click', () => {
+  event.preventDefault();
+  inputValidate();
+  testRadioChecked();
+  if (error > 0) {
+    document.querySelector('#personal-field').appendChild(messageParagraph);
+  }
 });
+
 function enableInputGenderOption() {
   const personalChecked = document.querySelector('#personal');
   personalChecked.setAttribute('type', 'text');
