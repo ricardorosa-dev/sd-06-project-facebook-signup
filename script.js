e46267613cceeb8f27e9a5cf52180408213302f1
Arthur de Sa <arthurdesa94@gmail.com>
2020-08-10 18:41:48 -0300

    adicionando o personalized gender

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ btnLogin.addEventListener('click', function () {
     alert(document.getElementById('user-email-phone').value);
   }
 });
+
+const personalized = document.querySelector("#personalizado");
+personalized.addEventListener("click", function(){
+    const personalizedOptionField = document.createElement("input");
+    personalizedOptionField.type = "text";
+    personalizedOptionField.name = "gender-custom";
+    personalizedOptionField.placeholder = "Gender (optional)";
+    const genderParentElement = document.querySelector(".other-fields");
+    genderParentElement.insertBefore(personalizedOptionField, document.querySelector("#facebook-register"));
+});
