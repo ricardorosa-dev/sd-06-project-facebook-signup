db98e9b2afc39fd4d7b01102213a67b1344f985c
Alvaro Neto <alvaroshn@gmail.com>
2020-08-10 17:02:24 -0300

    alteracoes para o item 20

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const genderOptionOther = document.getElementById('other');
 const genderOptionFemale = document.getElementById('female');
 const genderOptionMale = document.getElementById('male');
 const genderFather = document.getElementById('gender-area');
+const buttonRegister = document.getElementById('facebook-register');
+const getAllInputs = document.querySelectorAll('input');
+const radioSelection = document.querySelectorAll('input[name="gender"]');
+const rightContent = document.querySelector('.right-content');
+const genderSelection = document.getElementById('genders');
 
 genderOptionFemale.addEventListener('click', function () {
   genderFather.style.display = 'none';
@@ -17,3 +22,50 @@ genderOptionOther.addEventListener('click', function () {
 buttonLogin.addEventListener('click', function () {
   alert(inputEmailPhone.value);
 });
+buttonRegister.addEventListener('click', function (event) {
+  event.preventDefault();
+  if(checkAllConditions()){
+    getDivInformations(getAllInputs);
+  } 
+});
+function checkAllInputs(inputs) {
+  for (let i = 2 ; i < inputs.length - 1 ; i += 1) {
+    if(inputs[i].value === '') {
+      return false;
+    }
+  }
+  return true;
+}
+function getDivInformations(inputs) {
+  while (rightContent.firstChild) {
+    rightContent.removeChild(rightContent.firstChild);
+  }
+  const name = inputs[2].value;
+  const lastname = inputs[3].value;
+  const emailOrPhone = inputs[4].value;
+  const birthDate = inputs[6].value;
+  const genderChoice = inputs[getTheGender()+7].value;
+  const createNewParagraph = document.createElement('p');
+  const textMessage = "Olá, " +name +" " +lastname +", seu cadastro foi feito em: " +emailOrPhone +", nasceu na data: " +birthDate +".Possui o gênero " +genderChoice +".";
+  createNewParagraph.innerText = textMessage;
+  rightContent.appendChild(createNewParagraph);
+}
+function checkAllConditions() {
+  if (genderFather.style.display !== 'none' && genderSelection.value !== '0' && checkAllInputs(getAllInputs)) {
+    return true;
+  }
+  if (genderFather.style.display === 'none' && checkAllInputs(getAllInputs) ) {
+    for (let x = 0; x < radioSelection.length; x +=1 ) {
+      if(radioSelection[x].checked) {
+        return true;
+      }
+    }
+  }
+}
+function getTheGender(){
+  for (let j = 0; j < radioSelection.length; j += 1) {
+    if(radioSelection[j].checked) {
+    return j;
+    }
+  }
+}
