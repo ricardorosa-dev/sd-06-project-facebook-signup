934e7e271c385bc05ee2b6a8e0d6a3fd70503c0f
vbergadev <vbergadev@gmail.com>
2020-08-10 11:25:20 -0300

    form group container

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const entrar = document.getElementById('button-login');
 function entrada() {
-  let emailTelefone = document.getElementById('user-email-phone').value;
+  const emailTelefone = document.getElementById('user-email-phone').value;
   alert(emailTelefone);
 }
 entrar.addEventListener('click', entrada);
