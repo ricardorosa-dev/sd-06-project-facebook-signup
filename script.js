5db277a4414f73c6ae787ca8afe7a9d28b3f5165
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-10 11:03:22 -0300

    seventeenth and eighteenth requirements

--- script.js
+++ script.js
@@ -2,7 +2,33 @@ function loginButton() {
   alert(document.getElementById('user-email-phone').value);
 }
 
+function validate() {
+  const inputsArray = document.getElementsByTagName('input');
+
+  for (i = 0; i < inputsArray.length; i += 1) {
+
+    let radio = false;
+    if (inputsArray[i].type === 'radio') {
+      if (inputsArray[i].checked === true) {
+        radio = true;
+        console.log('radio');
+      }
+    } else {
+      if (inputsArray[i].value === '') {
+        alert ('Campos inválidos');
+        console.log(inputsArray[i]);
+        break;
+      }
+    }
+    if (radio === false) {
+      alert ('Campos inválidos');
+    }
+  }
+}
+
 window.onload = function () {
   const button = document.getElementById('button-login');
+  const registerButton = document.getElementById('facebook-register');
   button.addEventListener('click', loginButton);
+  registerButton.addEventListener('click', validate);
 };
