fbc92478828f49c5e0852d84b9e5a5e2273998b5
Dhiego Dias <ddias003@gmail.com>
2020-08-10 11:16:05 -0300

    Testes JS 18

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ const buttonLogin = document.querySelector('#button-login');
 buttonLogin.addEventListener('click', function () {
   alert(document.querySelector('#user-email-phone').value);
 });
+
+let fields = [];
+
+fields.push(document.querySelector('#firstname').value);
+fields.push(document.querySelector('#lastname').value);
+fields.push(document.querySelector('#phoneemail').value);
+fields.push(document.querySelector('#password').value);
+fields.push(document.querySelector('#birthdate').value);
+fields.push(document.querySelector('#gender').value);
+
+const facebookRegister = document.querySelector('#facebook-register');
+facebookRegister.addEventListener('click', function (event) {
+  event.preventDefault();
+  console.log(event)
+  const result = document.querySelector('#result');
+  for (let i = 0; i < fields.length; i += 1) {
+    if (fields[i] === '') {
+      result.innerHTML = 'Campos inválidos';
+    }
+  }
+});
