63cccb0a23a3fdef92f29321bf112634b293df1e
Alvaro Neto <alvaroshn@gmail.com>
2020-08-10 18:56:07 -0300

    alteracoes para o item 20 - codeclimate - campos invalidos para todos

--- script.js
+++ script.js
@@ -23,14 +23,15 @@ buttonLogin.addEventListener('click', function () {
   alert(inputEmailPhone.value);
 });
 function checkAllInputs(inputs) {
+  let cont = 0;
   for (let i = 2; i < inputs.length - 4; i += 1) {
     if (inputs[i].value === '') {
       inputs[i].type = 'text';
       inputs[i].value = 'Campos inválidos';
-      return false;
+      cont += 1;
     }
   }
-  return true;
+  return cont > 0 ? false : true;
 }
 function getTheGender() {
   for (let j = 0; j < radioSelection.length; j += 1) {
@@ -49,11 +50,8 @@ function getDivInformations(inputs) {
   const emailOrPhone = inputs[4].value;
   const birthDate = inputs[6].value;
   const genderChoice = inputs[getTheGender() + 7].value;
-  const createNewDiv = document.createElement('div');
-  createNewDiv.className = 'right-content';
   const textMessage = `Olá, ${name}  ${lastname} ,seu cadastro foi feito em: ${emailOrPhone} ,nasceu na data: ${birthDate} .Possui o gênero ${genderChoice}.`;
-  createNewDiv.innerHTML = textMessage;
-  rightContent.appendChild(createNewDiv);
+  rightContent.innerHTML = textMessage;
 }
 function checkFirstCondition() {
   for (let x = 0; x < radioSelection.length - 1; x += 1) {
