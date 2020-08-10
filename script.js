7c4a9ccf0cf56f42f799a7fedd0839e21b84db11
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-10 14:37:20 -0300

    Final Fixes 03

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let errors = 0;
 const info = [];
-const date = document.forms[1].elements[4];
 
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
