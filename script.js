f7419504da164cdcc168ae77ecc213bfd319df57
wfelipefarina <wfelipefarina@gmail.com>
2020-08-10 19:56:01 -0300

    add extra option gender script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const buttonLogin = document.querySelector('#button-login');
 const emailValue = document.querySelector('#user-email-phone');
+const form = document.querySelector('#formulary');
+const button = document.querySelector('#facebook-register')
 
 buttonLogin.addEventListener('click', function () {
   alert(emailValue.value);
 });
+
+const radios = document.getElementsByName('gender');
+const gender = document.querySelector('.gender');
+
+function addCampo() {
+    const inputGender = document.createElement('input');
+    inputGender.setAttribute('placeholder', 'Genero(opcional)');
+
+    for (let i = 0; i < radios.length; i += 1) {
+        radios[i].addEventListener('click', function () {
+            if (radios[i].value === 'Personalizado') {
+                form.insertBefore(inputGender, button);
+            } else {
+                inputGender.remove();
+            }
+        });
+    }
+}
+
+addCampo();
+
