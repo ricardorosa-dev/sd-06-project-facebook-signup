76537886a397bdf7b7548dfb65790bec7138af60
LeandroGnu <leandro.feitoza@gmail.com>
2020-08-10 00:03:51 -0300

    34- Corrigido erros apontados pelo CodeClimate

--- script.js
+++ script.js
@@ -4,30 +4,25 @@ btnLogIn.addEventListener('click', () => {
   alert(emailText.value);
 });
 
-function testRadioChecked () {
-  if (document.querySelector('#feminino').checked == false && 
-      document.querySelector('#masculino').checked == false &&
-      document.querySelector('#personalizado').checked == false) {
-        alert('Campos inválidos')
-      }
+function testRadioChecked() {
+  if (document.querySelector('#feminino').checked === false &&
+    document.querySelector('#masculino').checked === false &&
+    document.querySelector('#personalizado').checked === false) {
+    alert('Campos inválidos');
+  }
 }
 
-const inputsForm = document.querySelectorAll('.validaForm')
+const inputsForm = document.querySelectorAll('.validaForm');
 document.querySelector('#facebook-register').addEventListener('click', () => {
   testRadioChecked();
-  for(let i = 0; i <= inputsForm.length -1; i ++) {
-    console.log(inputsForm[i].value)
-    if(inputsForm[i].value == '') {
-      alert('Campos Inválidos')
+  for (let i = 0; i <= inputsForm.length - 1; i += 1) {
+    if (inputsForm[i].value === '') {
+      alert('Campos Inválidos');
     }
   }
-})
-
-
-
-document.querySelector('#personalizado').addEventListener('focus', enableInputGenderOption)
-
-function enableInputGenderOption () {
-  const personalChecked = document.querySelector('#personal')
-  personalChecked.setAttribute('type', 'text')
+});
+function enableInputGenderOption() {
+  const personalChecked = document.querySelector('#personal');
+  personalChecked.setAttribute('type', 'text');
 }
+document.querySelector('#personalizado').addEventListener('change', enableInputGenderOption);
