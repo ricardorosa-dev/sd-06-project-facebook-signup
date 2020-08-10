b88c0a66e8471e6383de07f821c1d2bd57381ce0
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-08-10 13:00:50 -0300

    Verificando o requisito 18 e 19

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btnLogin = document.querySelector('#button-login');
 const emailPhone = document.querySelector('#user-email-phone');
 const btnSignup = document.querySelector('#facebook-register');
 const registerForm = document.querySelector('#register-form');
+const custom = document.querySelector('#custom');
 
 btnLogin.addEventListener('click', function () {
   if (emailPhone.value !== '') {
@@ -11,13 +12,21 @@ btnLogin.addEventListener('click', function () {
   }
 });
 
-btnSignup.addEventListener('click', function () {
+btnSignup.addEventListener('click', function (e) {
   for (let i = 0; i < registerForm.children.length; i += 1) {
-    if (registerForm.children[i].tagName !== 'LABEL' && registerForm.children[i].tagName !== 'BUTTON') {
+    if (registerForm.children[i].required === true) {
       if (registerForm.children[i].value === '') {
         alert('Campos inválidos');
         break;
       }
     }
   }
+  e.preventDefault();
+});
+
+custom.addEventListener('click', function () {
+  const inputCustom = document.createElement('input');
+  inputCustom.name = 'gender-custom';
+  inputCustom.placeholder = 'Gênero (opcional)';
+  registerForm.insertBefore(inputCustom, registerForm.children[registerForm.children.length - 1]);
 });
