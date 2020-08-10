096c0b12b8ab71bcf9fb3658901c05f1d6cd0dd1
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-08-10 08:13:37 -0300

    Organização do arquivo script.js

--- script.js
+++ script.js
@@ -21,13 +21,12 @@ function createTableInfo() {
   const birthday = document.querySelector('.birthdate').value;
   // recupera o valor do radio button , precisa fazer teste para saber ql esta marcado
   const valido = document.getElementsByName('gender');
-  const gender;
   if (valido[0].checked) {
-    gender = valido[0].value;
+    const gender = valido[0].value;
   } else if (valido[1].checked) {
-    gender = valido[1].value;
+    const gender = valido[1].value;
   } else if (valido[2].checked) {
-    gender = document.getElementById('gender-custom-id').value;
+    const gender = document.getElementById('gender-custom-id').value;
   }
   // recupera o pai para a gente add a nova rightcontent
   const pai = document.querySelector('.main-content');
