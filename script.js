6d72dacf44173c7cd765c8989564aec87e789ad2
Matheus Dias <matheuscara2000@gmail.com>
2020-08-11 14:20:25 -0300

    Atualizando o code-climate!

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ const botao = document.querySelector('#button-login');
 const inputEmailPhone = document.querySelector('#user-email-phone');
 botao.addEventListener('click', function () {
   alert(inputEmailPhone.value);
-  }
-);
+});
