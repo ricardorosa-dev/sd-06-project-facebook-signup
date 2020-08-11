9638b43f2630341ea1a930d7e17966d5db33c3b9
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-08-11 14:00:10 -0300

    adiconando função para criar novo input

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const btnLogin = document.querySelector('#button-login');
 const inputBox = document.querySelector('#user-email-phone');
+const genderCuston = document.querySelector('#gender-person');
 
 btnLogin.addEventListener('click', function () {
   alert(inputBox.value);
 });
+
+genderCuston.addEventListener('checked', function () {
+  let newInput = document.createElement('input');
+  newInput.setAttribute('type', 'text');
+  newInput.setAttribute('name', 'gender-custon');
+  newInput.setAttribute('placeholder', 'Gênero (opcional)');
+  document.getElementById('input-gender-custon').appendChild(newInput);
+})
