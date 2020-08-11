9211ea9100505e62042a2b87c4c44512a7ea4b7f
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-08-11 16:06:06 -0300

    constante customRadio

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const buttonLogin = document.getElementById('button-login');
 const userLoginEmailPhone = document.getElementById('user-email-phone');
+const customRadio = document.getElementById('custom');
 
 function clickButton() {
     alert(userLoginEmailPhone.value);
   }
 
+function customOption() {
+    const createCustom = document.createElement('input');
+
 window.onload = function () {
     buttonLogin.addEventListener('click', clickButton);
+    customRadio.addEventListener('change', customOption);
   };
