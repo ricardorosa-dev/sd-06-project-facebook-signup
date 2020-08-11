5953d13192e33159f5257236f6ba8355e4845e0c
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-11 12:37:30 -0300

    Controle dos campos input no form

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-window.onload = function () {
-  const buttonLogin = document.querySelector('#button-login');
-  buttonLogin.addEventListener('click', function () {
-    const inputUser = document.querySelector('input#user-email-phone').value;
-    alert(inputUser);
-  });
+const buttonLogin = document.querySelector('#button-login');
+buttonLogin.addEventListener('click', function () {
+  const inputUser = document.querySelector('input#user-email-phone').value;
+  alert(inputUser);
+});
 
-  const buttonRegister = document.querySelector('#facebook-register');
-  buttonRegister.addEventListener('click', formValues, {
-  });
-
-  function formValues () {
-    const valuesInput = document.querySelectorAll('.right-content input');
-    if (chechRadio () === true) {
-      let valueUndefined = false;
-      for (let i = 0; i < valuesInput.length - 3; i += 1) {
-        if (valuesInput[i].values === undefined) {
-          valueUndefined = true;
-        }
-      }
-      if (valueUndefined === true) {
-        putMenssagem ();
-      }
-    }
-    else {
-      putMenssagem ();
-    }
-  };
-
-  function chechRadio () {
-    let valueGender = false;
-    let gender = document.querySelectorAll('.gender');
-    for (let i = 0; i < gender.length; i += 1) {
-      if (gender[i].checked === true) {
-        valueGender = true;
-      } 
-    }
-    return valueGender;
-  };
-};
-
-function putMenssagem () {
-  const mensagem = document.createElement('p');
+function putMenssagem() {
+  let mensagem = document.createElement('p');
   mensagem.innerHTML = 'Campos inválidos';
   document.querySelector('#form').appendChild(mensagem);
-};
+}
+
+function formValues() {
+  const valuesInput = document.querySelectorAll('right-content input');
+  let valueUndefined = false;
+  for (let i = 0; i < valuesInput.length - 3; i += 1) {
+    if (valuesInput[i].value === "") {
+      valueUndefined = true;
+    }
+  }
+  if (valueUndefined === true) {
+    putMenssagem();}
+}
+
+function chechRadio() {
+  let valueGender = false;
+  let gender = document.querySelectorAll('.gender');
+  for (let i = 0; i < gender.length; i += 1) {
+    if (gender[i].checked === true) {
+      valueGender = true;
+    }
+  }
+  return valueGender;
+}
+
+const buttonRegister = document.querySelector('#facebook-register');
+buttonRegister.addEventListener('click', function () {
+  if (chechRadio() === true) {
+    formValues()
+  }
+  else {
+    putMenssagem();
+  }
+});
