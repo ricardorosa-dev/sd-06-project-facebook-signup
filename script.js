fa2d1f2261fe5a365173552744814e2476302963
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-08-11 12:32:34 -0300

    validação de campos - requisito 18

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const buttonLogin = document.querySelector('#button-login');
-const emailPhoneInput = document.querySelector('#user-email-phone');
 
 // window.onload = function () {
 //   let picker = new Pikaday({
@@ -7,6 +5,42 @@ const emailPhoneInput = document.querySelector('#user-email-phone');
 //   });
 // };
 
+const emailPhoneInput = document.querySelector('#user-email-phone');
+const buttonLogin = document.querySelector('#button-login');
 buttonLogin.addEventListener('click', function () {
   alert(emailPhoneInput.value);
+});
+
+function validateInputs(inputs) {
+  let validate = true;
+  for (let index = 0; index < inputs.length; index += 1) {
+    if ((inputs[index].type === 'text') && (inputs[index].value === '')) {
+      validate = false;
+    }
+  }
+  return validate;
+}
+
+function checkRadios(inputs) {
+  let radioChecked = false;
+  for (let index = 0; index < inputs.length; index += 1) {
+    if ((inputs[index].type === 'radio') && (inputs[index].checked)) {
+      radioChecked = true;
+    }
+  }
+  return radioChecked;
+}
+
+const buttonSubmit = document.querySelector('#facebook-register');
+buttonSubmit.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const registrationForm = document.querySelector('.registration-form');
+  const inputs = registrationForm.getElementsByTagName('input');
+  const divError = document.querySelector('.error');
+  if ((!validateInputs(inputs)) || (!checkRadios(inputs))) {
+    divError.innerHTML = 'Campos inválidos';
+  } else {
+    divError.innerHTML = '';
+  }
 });
