9fb8a51af8491a143d7433fe0d9e901c42b8faf1
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-11 11:58:13 -0300

    Controle dos campos input no form

--- script.js
+++ script.js
@@ -4,4 +4,43 @@ window.onload = function () {
     const inputUser = document.querySelector('input#user-email-phone').value;
     alert(inputUser);
   });
+
+  const buttonRegister = document.querySelector('#facebook-register');
+  buttonRegister.addEventListener('click', formValues, {
+  });
+
+  function formValues () {
+    const valuesInput = document.querySelectorAll('.right-content input');
+    if (chechRadio () === true) {
+      let valueUndefined = false;
+      for (let i = 0; i < valuesInput.length - 3; i += 1) {
+        if (valuesInput[i].values === undefined) {
+          valueUndefined = true;
+        }
+      }
+      if (valueUndefined === true) {
+        putMenssagem ();
+      }
+    }
+    else {
+      putMenssagem ();
+    }
+  };
+
+  function chechRadio () {
+    let valueGender = false;
+    let gender = document.querySelectorAll('.gender');
+    for (let i = 0; i < gender.length; i += 1) {
+      if (gender[i].checked === true) {
+        valueGender = true;
+      } 
+    }
+    return valueGender;
+  };
+};
+
+function putMenssagem () {
+  const mensagem = document.createElement('p');
+  mensagem.innerHTML = 'Campos inválidos';
+  document.querySelector('#form').appendChild(mensagem);
 };
