03a4cd384a56cecdc250cea798b5c482ee1a0750
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-08-11 17:43:58 -0300

    JS

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const buttonLogin = document.getElementById('button-login');
 const userLoginEmailPhone = document.getElementById('user-email-phone');
 const customRadio = document.getElementById('custom');
+const invalidMessage = document.getElementById('invalid-input');
+const btnSingUp = document.getElementById('facebook-register');
+const genderCustom = document.querySelector('.custom-option');
 
 function clickButton() {
     alert(userLoginEmailPhone.value);
   }
 
+  function customOption() {
+    const createCustom = document.createElement('input');
+    createCustom.name = 'gender-custom';
+    createCustom.placeholder = 'Gênero (opcional)';
+    genderCustom.appendChild(createCustom);
+  }
+  
 window.onload = function () {
     buttonLogin.addEventListener('click', clickButton);
     customRadio.addEventListener('change', customOption);
