4876e1d08c1fabacd2114de699219ec9b9299645
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-08-11 15:31:33 -0300

    JS const userLoginEmailPhone e function

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const buttonLogin = document.getElementById('button-login');
+const userLoginEmailPhone = document.getElementById('user-email-phone');
+
+function clickButton() {
+    alert(userLoginEmailPhone.value);
+  }
+
 window.onload = function () {
     buttonLogin.addEventListener('click', clickButton);
   };
