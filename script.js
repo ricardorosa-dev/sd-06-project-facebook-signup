5bbe387985c1c0995ac0885380920888edec2c4e
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-08-11 17:31:42 -0300

    ajustes CSS e JS

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 const buttonLogin = document.getElementById('button-login');
 const userLoginEmailPhone = document.getElementById('user-email-phone');
 const customRadio = document.getElementById('custom');
-const genderCustom = document.querySelector('.custom-option');
 
 function clickButton() {
     alert(userLoginEmailPhone.value);
   }
 
-function customOption() {
-    const createCustom = document.createElement('input');
-    createCustom.name = 'gender-custom';
-    genderCustom.appendChild(createCustom);
-
 window.onload = function () {
     buttonLogin.addEventListener('click', clickButton);
     customRadio.addEventListener('change', customOption);
 }
-
