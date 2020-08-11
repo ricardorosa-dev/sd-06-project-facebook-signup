a1ac8387056f0532bee6dcd1495deeffc816976a
Sttefanie Maciel <macielsttefanie@gmail.com>
2020-08-11 10:58:11 -0300

    adicionado comentarios no js

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-/* document.getElementById('button-login').addEventListener('click', function () {
-  alert(document.getElementById('user-email-phone').value);
-});
+/*
+  mapeando elementos da pagina e colocando em constantes
 */
+
 const btnLogin = document.getElementById('button-login');
 const userEmailPhone = document.getElementById('user-email-phone');
 const customRadio = document.getElementById('custom');
 const genderCustom = document.querySelector('.custom-option');
 
+// exibindo um alerta ao clicar em um botao
 function clickButton() {
+  // exibe uma mensagem com o valor do email-phone
   alert(userEmailPhone.value);
 }
 
+// exibe um text-input depois que é clicado na opção personalizado
 function customOption() {
+  // cria um elemento text-input na página
   const createCustom = document.createElement('input');
+  // adiconando atributos no text-input
   createCustom.name = 'gender-custom';
   createCustom.placeholder = 'Gênero (opcional)';
+  // mostrando na pagina
   genderCustom.appendChild(createCustom);
 }
 
 window.onload = function () {
+  // adicionando um evento no elemento btnLogin
   btnLogin.addEventListener('click', clickButton);
+  // adicionando um evento no elemento customRadio
   customRadio.addEventListener('change', customOption);
 };
