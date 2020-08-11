37609b35920056e7067b5e10b993d663afed7a79
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-11 20:59:06 -0300

    twentieth requirement

--- script.js
+++ script.js
@@ -38,6 +38,10 @@ function validate(event) {
     p.innerText = 'Campos inválidos';
     document.getElementById('validateDiv').appendChild(p);
   }
+  else {
+    saveContent ();
+    newContent ();
+  }
 }
 
 function otherButton() {
@@ -55,6 +59,56 @@ function otherButton() {
     divOther.appendChild(input);
   }
 }
+
+function saveContent () {
+  let firstName = document.getElementById('firstname').value;
+  let lastName = document.getElementById('lastname').value;
+  let phone_email = document.getElementById('phone_email').value;
+  let birthDate = document.getElementById('birthdate').value;
+  let radioButtonOption = document.querySelectorAll('form')[1].querySelectorAll('input[type=radio]');
+  let radioButtonOptionSave
+  for(i = 0; i < radioButtonOption.length; i++) { 
+    if(radioButtonOption[i].checked) 
+    radioButtonOptionSave = radioButtonOption[i].value
+  }
+  localStorage.setItem('firstName', firstName);
+  localStorage.setItem('lastName', lastName);
+  localStorage.setItem('phone_email', phone_email);
+  localStorage.setItem('birthdate', birthDate);
+  localStorage.setItem('gender', radioButtonOptionSave);
+}
+
+function newContent () {
+  let divRight = document.getElementsByClassName('right-content');
+  const divValidate = document.getElementById('validateDiv');
+  if (divValidate !== '') {
+    divRight.innerHTML = '';
+
+    let helloText = document.createElement('p');
+    let firstName = localStorage.getItem('firstName');
+    let lastName = localStorage.getItem('lastName');
+
+    helloText.innerText = `Olá, ${firstName} ${lastName}`;
+    divRight.appendChild(helloText);
+    
+    let johnDoeTelephone = document.createElement('p');
+    let phone_email = localStorage.getItem('phone_email');
+    johnDoeTelephone.innerText = phone_email.value;
+    divRight.appendChild(johnDoeTelephone);
+
+    let johnDoeBirthDate = document.createElement('p');
+    let birthdate = localStorage.getItem('birthdate');
+    johnDoeBirthDate.innerText = birthdate.value;
+    divRight.appendChild(johnDoeBirthDate);
+
+    let johnDoeGender = document.createElement('p');
+    let gender = localStorage.getItem('gender');
+    johnDoeGender.innerText = gender.value;
+    divRight.appendChild(johnDoeGender);
+    
+  }
+}
+
 
 window.onload = function () {
   const button = document.getElementById('button-login');
