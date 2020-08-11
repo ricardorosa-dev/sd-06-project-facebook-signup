f0e91d91467130bf0eb805c5be3a737e52e9b33a
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-08-11 15:41:27 -0300

    Fixing bugs - Step 20

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function genderFieldFunc() {
 }
 
 function insertContent(list) {
-  document.querySelector('.right-content').innerHTML = `Olá, ${list[0]} ${list[1]} , e-mail ${list[2]}, nascido em ${list[4]}, gênero ${list[5]}`;
+  document.querySelector('.right-content').innerHTML = `Olá, ${list[0]} ${list[1]} ${list[2]} ${list[4]} ${list[5]}`;
   // const p = document.createElement('p');
   // p.innerHTML = `Olá, ${list[0]} ${list[1]} , e-mail ${list[2]},
   // nascido em ${list[4]}, gênero ${list[5]}`;
