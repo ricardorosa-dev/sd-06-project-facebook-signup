576d15a84317b726f2f5f82dabf981405318dd5e
Daniele Perse <danieleperse3@gmail.com>
2020-08-11 11:21:08 -0300

    add requisito 19

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const button = document.getElementById('button-login');
+const buttonLogin = document.getElementById('button-login');
 const inputEmail = document.getElementById('user-email-phone');
+const clickPersonalizado = document.getElementsByClassName('gender')[2];
+const divNewOption = document.getElementById('new-option');
 
 function buttonAlert() {
   alert(inputEmail.value);
 }
 
+function newOption() {
+  const newElement = document.createElement('input');
+  newElement.type = 'text';
+  newElement.name = 'gender-custom';
+  newElement.placeholder = 'Gênero (opcional)';
+  divNewOption.appendChild(newElement);
+}
+
 window.onload = function () {
-  button.addEventListener('click', buttonAlert);
+  buttonLogin.addEventListener('click', buttonAlert);
+  clickPersonalizado.addEventListener('click', newOption);
 };
