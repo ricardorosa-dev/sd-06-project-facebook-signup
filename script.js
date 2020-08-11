8be3df043dc4ac642f8c223a6e2ae46de259f3a8
Zeca Costa <jccostaso@gmail.com>
2020-08-11 15:01:18 -0300

    corrigir requisitos gender-custom e substituir conteúdo #2

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+// função para tratar evento do botão para exibir alert
 const buttonLogin = document.getElementById('button-login');
 buttonLogin.addEventListener('click', function () {
   const inputEmail = document.getElementById('user-email-phone').value;
   window.alert(inputEmail);
   inputEmail.innerHTML = '';
 });
-
+// CONJUNTO DE FUNÇÕES PARA TRATAR GENDER-CUSTON
+// criar elemento que receberá texto
 function createInput() {
   const inputElement = document.createElement('input');
+  inputElement.type = 'text';
   inputElement.name = 'gender-custom';
   inputElement.placeholder = 'Gênero (opcional)';
   inputElement.className = 'gender-custom';
   return inputElement;
 }
-
+// adicionar elemento quando opção personalizado e já não existir
 function addInput() {
   const extraGender = document.querySelector('#input-personalizado');
 
@@ -21,14 +24,14 @@ function addInput() {
     extraGender.appendChild(inputElement);
   }
 }
-
+// remover elemento quando opção diferente de personalizado
 function removeInput() {
   const extraGender = document.querySelector('#input-personalizado');
   if (extraGender.hasChildNodes()) {
     extraGender.removeChild(extraGender.firstChild);
   }
 }
-
+// tratar addEventListener, testar se personalizado chamando função adequada
 function handleGenderOption() {
   const customize = document.getElementById('personalizado');
   if (customize.checked) {
@@ -37,9 +40,11 @@ function handleGenderOption() {
     removeInput();
   }
 }
+// escutar addEventListener e chamar a função que vai tratar
 const genderOption = document.querySelector('.form-group-4');
 genderOption.addEventListener('change', handleGenderOption);
-
+// CONJUNTO DE FUNÇÕES PARA OBTER VALORES DOS INPUTS
+// obter valores dos inputs tipo text da right-content
 const getText = () => {
   const allText = [];
   const inputText = document.querySelectorAll('.selected-input');
@@ -54,7 +59,7 @@ const getText = () => {
   }
   return allText;
 };
-
+// obter valor do input tipo password da right-content
 const getPassword = () => {
   const allPassword = [];
   const inputPassword = document.querySelector('#password').value;
@@ -66,7 +71,7 @@ const getPassword = () => {
   }
   return allPassword;
 };
-
+// obter valores dos inputs tipo radio da right-content
 const getRadio = () => {
   const allRadio = [];
   const inputRadio = document.querySelectorAll('input[type=radio]');
@@ -74,7 +79,7 @@ const getRadio = () => {
   let count = 1;
   for (let i = 0; i < inputRadio.length; i += 1) {
     if (inputRadio[i].checked) {
-      const radio = inputRadio[i].labels[0].innerHTML;
+      const radio = inputRadio[i].value;
       allRadio.push(radio);
       count -= 1;
     }
@@ -85,7 +90,7 @@ const getRadio = () => {
   }
   return allRadio;
 };
-
+// obter valor do input tipo date da right-content
 const getDate = () => {
   const allDate = [];
   const inputDate = document.querySelector('input[type=date]').value;
@@ -97,20 +102,21 @@ const getDate = () => {
   }
   return allDate;
 };
-
+// CONJUNTO DE FUNÇÕES PARA MONTAR E APRESENTAR MENSAGENS
+// substituir conteúdo pela mensagem na div right-content
 const renderOutput = (input) => {
   const rightContentDiv = document.querySelector('.right-content');
 
   rightContentDiv.innerHTML = '';
   rightContentDiv.innerHTML = `<p>${input}</p>`;
 };
-
+// apresentar mensagem de campos inválidos
 const renderMessage = (input) => {
   const renderMessageDiv = document.querySelector('#render-message');
   renderMessageDiv.classList.remove('hidden');
   renderMessageDiv.innerHTML = `<span>${input}</span>`;
 };
-
+// escutar addEventListener do botão, testar se inputs OK e montar mensagens.
 const submitBtn = document.querySelector('#facebook-register');
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault();
