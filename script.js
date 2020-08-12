8aa731793d30919a95e0c538a876a7237c200b7d
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-08-12 11:58:17 -0300

    requisito 20

--- script.js
+++ script.js
@@ -19,15 +19,39 @@ submitButton.addEventListener('click', function () {
       paragraph.innerHTML = 'Campos inválidos';
       document.querySelector('#register-form').appendChild(paragraph);
       break;
+    } else {
+      storageValues(inputs[index]);
     }
   }
+  criarDiv();
 });
 
 genderCustom.onclick = () => {
+  const customSellection = document.querySelector('input [name = "gender-custom"]');
+  if (customSellection) {
+    customSellection.remove();
+  }
   const genderDiv = document.createElement('input');
   genderDiv.type = 'text';
   genderDiv.name = 'gender-custom';
   genderDiv.placeholder = 'Gênero (opcional)';
   genderDiv.className = 'display-block';
   document.querySelector('#register-form').insertBefore(genderDiv, submitButton);
+};
+
+function storageValues (param) {
+  if (param.type != 'radio') {
+    localStorage.setItem(param.name, param.value);
+  } else if (param.checked) {
+    localStorage.setItem(param.name, param.value);
+  }
+};
+
+function criarDiv() {
+  const rightContent = document.querySelector('.right-content');
+  rightContent.innerHTML = `<h2>Olá, ${localStorage.getItem('firstname')} ${localStorage.getItem('lastname')}</h2> 
+  
+  <p>${localStorage.getItem('phone_email')}</p>
+  <p>${localStorage.getItem('birthdate')}</p>
+  <p>${localStorage.getItem('gender')}</p>`;
 };
