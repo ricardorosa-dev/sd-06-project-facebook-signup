2635e1a7a048a083711e92243836dfb52c30907b
Zeca Costa <jccostaso@gmail.com>
2020-08-12 07:36:37 -0300

    corrigir requisito #20 (substituir conteudo right-content) #4

--- script.js
+++ script.js
@@ -91,25 +91,26 @@ const getRadio = () => {
   return allRadio;
 };
 // formatar a data padrão yyyy-MM-dd.
-const parseDate = (date) => {
-  const day = date.slice(-2);
-  const month = date.slice(5, 7);
-  const year = date.slice(0, 4);
-  const formatDate = `${year}-${month}-${day}`;
-  return formatDate;
-};
+// const parseDate = (date) => {
+//   const day = date.slice(-2);
+//   const month = date.slice(5, 7);
+//   const year = date.slice(0, 4);
+//   const formatDate = `${year}-${month}-${day}`;
+//   return formatDate;
+// };
 // obter valor do input tipo date da right-content.
-const getDate = () => {
-  const allDate = [];
-  const inputDate = document.querySelector('input[type=date]').value;
-  if (inputDate === null || inputDate === '') {
-    allDate.push(null);
-  } else {
-    const formatDate = parseDate(inputDate);
-    allDate.push(formatDate);
-  }
-  return allDate;
-};
+// const getDate = () => {
+//   const allDate = [];
+//   const inputDate = document.querySelector('input[type=date]').value;
+//   if (inputDate === null || inputDate === '') {
+//     allDate.push(null);
+//   } else {
+//     // const formatDate = parseDate(inputDate);
+//     // allDate.push(formatDate);
+//     allDate.push(inputDate);
+//   }
+//   return allDate;
+// };
 // CONJUNTO DE FUNÇÕES PARA MONTAR E APRESENTAR AS MENSAGENS OK OU NOK.
 // substituir conteúdo pela mensagem de OK na div right-content.
 const renderOutput = (input) => {
@@ -132,8 +133,9 @@ submitBtn.addEventListener('click', (event) => {
   const allText = getText();
   const allPassword = getPassword();
   const allRadio = getRadio();
-  const allDate = getDate();
-  const allInputs = allText.concat(allPassword, allRadio, allDate);
+  // const allDate = getDate();
+  // const allInputs = allText.concat(allPassword, allRadio, allDate);
+  const allInputs = allText.concat(allPassword, allRadio);
 
   let testResult = 0;
   for (let i = 0; i < allInputs.length; i += 1) {
@@ -146,7 +148,7 @@ submitBtn.addEventListener('click', (event) => {
     const MessageString = 'Campos inválidos';
     renderMessage(MessageString);
   } else {
-    const okMessage = `Olá ${allInputs[0]} ${allInputs[1]}, seu email ou telefone é ${allInputs[2]}, selecionou o gênero ${allInputs[4]} e se inscreveu em ${allInputs[5]}.`;
+    const okMessage = `Olá ${allInputs[0]} ${allInputs[1]}, você nasceu em ${allInputs[3]}, seu email ou telefone é ${allInputs[2]} e selecionou o gênero ${allInputs[5]}.`;
     renderOutput(okMessage);
   }
 });
