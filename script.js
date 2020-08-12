be536ab9af6b8d80227c6e4aee68f1c05000338e
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-08-12 20:41:41 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 const buttonLogin = document.getElementById('button-login');
 const userEmailPhone = document.getElementById('user-email-phone');
+const userPassword = document.getElementById('user-password');
+const nome = document.getElementById('nome');
+const sobrenome = document.getElementById('sobrenome');
+const celularEmail = document.getElementById('celularEmail');
 const customRadio = document.getElementById('custom');
-const genderCustom = document.querySelector('.custom-option');
 
 function clickButton() {
   alert(userEmailPhone.value);
 }
 
-function customOption() {
-  const createCustom = document.createElement('input');
-  createCustom.name = 'gender-custom';
-  createCustom.placeholder = 'Gênero (opcional)';
-  genderCustom.appendChild(createCustom);
-}
-
 window.onload = function () {
   buttonLogin.addEventListener('click', clickButton);
-  customRadio.addEventListener('change', customOption);
-};
+  };
