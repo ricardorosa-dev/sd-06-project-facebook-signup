4b338a8c6c0185b365dedb14df7b99fc697683a3
LeonardoCavachini <cavachini240@gmail.com>
2020-08-11 22:59:46 -0300

    item 19

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ btn.addEventListener('click', function () {
     }
   }
 });
+const perso = document.getElementById('others');
+const localPerso = document.createElement('input');
+localPerso.name = 'genre-custom';
+localPerso.placeholder = 'Gênero(opcional)';
+const contaiGender = document.getElementsByClassName('gender-container')[0];
+perso.addEventListener('click', function () {
+  contaiGender.appendChild(localPerso);
+  contaiGender.insertBefore(localPerso, btn);
+});
