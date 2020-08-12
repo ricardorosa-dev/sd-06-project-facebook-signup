16e58c64fc4d6e45a537a4aed2aac5abf973ea9d
William <williamjog@hotmail.com>
2020-08-11 23:40:53 -0300

    18th requirement

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 const loginButton = document.querySelector('#button-login');
 const emailInput = document.querySelector('#user-email-phone');
+const allInputFields = document.querySelectorAll('.signUp');
+const allRadioButtons = document.querySelectorAll('.signUpButton');
+const signUpButton = document.querySelector('#facebook-register');
+
+function checkAllSignUpFields() {
+  for (let i = 0; i < allInputFields.length; i += 1) {
+    if (allInputFields[i].value === '') {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkAllRadioButtons() {
+  for (let i = 0; i < allRadioButtons.length; i += 1) {
+    if (allRadioButtons[i].checked === false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function createInvalidFieldsMessage() {
+  const invalidFields = document.createElement('span');
+  document.querySelector('.gender').appendChild(invalidFields);
+  invalidFields.innerText = 'Campos inválidos';
+  invalidFields.style.color = 'red';
+  invalidFields.classList.add('invalidFields');
+}
 
 loginButton.addEventListener('click', () => {
   alert(emailInput.value);
+});
+
+signUpButton.addEventListener('click', (event) => {
+  if (!checkAllSignUpFields() || !checkAllRadioButtons()) {
+    event.preventDefault();
+    const invalidFields = document.querySelector('.invalidFields');
+    if (!invalidFields) {
+      createInvalidFieldsMessage();
+    }
+  }
 });
