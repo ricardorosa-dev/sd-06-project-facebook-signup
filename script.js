8f529ba3b1eeac0463c843b28a94ed171e07cb2a
FlavioSugano <flavio.sugano@gmail.com>
2020-08-13 02:52:00 -0300

    Formulário da opção de gênero personalizado

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 window.onload = function () {
   const buttonLogin = document.querySelector('#button-login');
+  const radioButtonFemaleGender = document.querySelector('#gender-female');
+  const radioButtonMaleGender = document.querySelector('#gender-male');
+  const radioButtonCustomGender = document.querySelector('#gender-custom');
+
+
   buttonLogin.addEventListener('click', function () {
     const inputUser = document.querySelector('input#user-email-phone').value;
     alert(inputUser);
+  });
+
+  radioButtonFemaleGender.addEventListener('click', function () {
+    document.getElementById('gender-custom-form').style.display = 'none';
+  });
+
+  radioButtonMaleGender.addEventListener('click', function () {
+    document.getElementById('gender-custom-form').style.display = 'none';
+  });
+
+  radioButtonCustomGender.addEventListener('click', function () {
+    document.getElementById('gender-custom-form').style.display = 'block';
   });
 };
