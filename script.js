48df3a913ea607e0c293b724e1d7cdee75afeef7
Alana Monteiro <lanaah.15@gmail.com>
2020-08-13 14:06:06 -0300

    novo campo para genero personalizado

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const loginButton = document.querySelector('#button-login');
 const userEmailPhone = document.querySelector('#user-email-phone');
+const customGender = document.querySelector('#custom');
+
 
 //  Consulta realizada no PullRequest https://github.com/tryber/sd-06-project-facebook-signup/pull/117/files como referência
 
@@ -7,6 +9,18 @@ function clickButton() {
   alert(userEmailPhone.value);
 }
 
+function customField() {
+  let genderParent = document.querySelector('.new-gender-field');
+  let newGender = document.createElement('input');
+  genderParent.appendChild(newGender);
+  newGender.setAttribute('type', 'text');
+  newGender.setAttribute('name', 'gender-custom');
+  newGender.setAttribute('placeholder', 'Gênero (opcional)');
+  newGender.setAttribute('type', 'text');
+}
+
 window.onload = function () {
   loginButton.addEventListener('click', clickButton);
 };
+
+customGender.addEventListener ('change', customField);
