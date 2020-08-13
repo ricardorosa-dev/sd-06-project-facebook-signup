116face64735bf1d0b090f9b80445ba9a656a334
Anderson Alves <contatodeanderson@gmail.com>
2020-08-13 15:25:14 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 //  Variáveis
 const buttonEnter = document.querySelector('#button-login');
-// const nameInput = document.querySelector('#firstname');
-// const lastnameInput = document.querySelector('#lastname');
-// const phoneOrEmailInput = document.querySelector('#phone_email');
-// const passwordInput = document.querySelector('#password');
-// const birthdateInput = document.querySelector('#birthdate');
-// const femaleInput = document.querySelector('#gender-female');
-// const maleInput = document.querySelector('#gender-male');
-// const personalizedInput = document.querySelector('#gender-personalized');
-// const formRegister = document.querySelector('#form-register');
+const nameInput = document.querySelector('#firstname');
+const lastnameInput = document.querySelector('#lastname');
+const phoneOrEmailInput = document.querySelector('#phone_email');
+const birthdateInput = document.querySelector('#birthdate');
+const radioButtons = document.querySelectorAll('.radio-options');
 const buttonRegister = document.querySelector('#facebook-register');
+const mainContentDiv = document.querySelector('.main-content');
+const inputElements = document.querySelectorAll('.input-element');
 
 //  Criar um alerta ao clicar em 'Entrar'
 buttonEnter.addEventListener('click', function () {
@@ -17,10 +15,31 @@ buttonEnter.addEventListener('click', function () {
   alert(nameOrPhone);
 });
 
+// Retornar o genero escolhido
+function getRadioValue() {
+  for (let index = 0; index < radioButtons.length; index += 1) {
+    if (radioButtons[index].checked) {
+      return radioButtons[index].value;
+    }
+  }
+  return false;
+}
+
+// Armazenar os dados submetidos pelo usuario
+function completedForm() {
+  const dataSubmittedDiv = document.createElement('div');
+  dataSubmittedDiv.className = 'right-content';
+  dataSubmittedDiv.innerText = `Olá, ${nameInput.value} ${lastnameInput.value}
+  ${phoneOrEmailInput.value}
+  ${birthdateInput.value}
+  ${getRadioValue()}
+  `;
+  mainContentDiv.appendChild(dataSubmittedDiv);
+}
+
 // Exibir uma mensagem de "Campos inválidos" se pelo menos um campo não esteja preenchido
 buttonRegister.addEventListener('click', function (e) {
   e.preventDefault();
-  const inputElements = document.querySelectorAll('.input-element');
   for (let index = 0; index < inputElements.length; index += 1) {
     if (inputElements[index].value === '') {
       const messageDiv = document.querySelector('.empty-input');
@@ -28,9 +47,10 @@ buttonRegister.addEventListener('click', function (e) {
       return false;
     }
   }
+  const rightContentDiv = document.querySelector('.right-content');
+  rightContentDiv.remove();
+  completedForm();
   return true;
-//  const rightContentDiv = document.querySelector('.right-content');
-//  rightContentDiv.remove();
 });
 
 // Exibir um capo de texto caso o usuário clique em "personalizado"
