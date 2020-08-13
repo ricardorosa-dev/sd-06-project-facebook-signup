f3296e93d00e18d530149c4f4314a4f1811ad893
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-13 11:24:00 -0300

    Fix name of login password

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function validateForm() {
   const firstName = newUser.elements.firstname.value;
   const lastName = newUser.elements.lastname.value;
   const phoneEmail = newUser.elements.phone_email.value;
-  const password = newUser.elements.passwordContent.value;
+  const password = newUser.elements.password.value;
   const birthdate = newUser.elements.birthdate.value;
   const gender = newUser.elements.gender.value;
   const allInputsRequired = [firstName, lastName, phoneEmail, password, birthdate, gender];
