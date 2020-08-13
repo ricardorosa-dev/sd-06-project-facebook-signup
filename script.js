3c731ed75e4486c97fa822df8ac8b7f319d76d76
Alana Monteiro <lanaah.15@gmail.com>
2020-08-13 11:47:05 -0300

    codeclimate

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const userEmailPhone = document.getElementById('user-email-phone');
 // const customRadio = document.getElementById('custom');
 
 
-//Consulta realizada no PullRequest https://github.com/tryber/sd-06-project-facebook-signup/pull/117/files como referência
+//  Consulta realizada no PullRequest https://github.com/tryber/sd-06-project-facebook-signup/pull/117/files como referência
 
 function clickButton() {
   alert(userEmailPhone.value);
