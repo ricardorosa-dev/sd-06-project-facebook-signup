f9d0b83c386d55a89d449acdc9ed8523440f3c6d
Daniele Perse <danieleperse3@gmail.com>
2020-08-13 10:02:42 -0300

    alterei a posiçao do link script no html, agora esta no head. Com isso corrigi a posiçao de todas as contantes do meu js, inserindo nas respectivas funçoes. Criei a funçao dataValidation

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const buttonLogin = document.getElementById('button-login');
-const inputEmail = document.getElementById('user-email-phone');
-const clickPersonalizado = document.getElementsByClassName('gender')[2];
-const divNewOption = document.getElementById('new-option');
-
 function buttonAlert() {
+  const inputEmail = document.getElementById('user-email-phone');
   alert(inputEmail.value);
 }
 
 function newOption() {
+  const divNewOption = document.getElementById('new-option');
   const newElement = document.createElement('input');
   newElement.type = 'text';
   newElement.name = 'gender-custom';
@@ -15,7 +12,25 @@ function newOption() {
   divNewOption.appendChild(newElement);
 }
 
+function dataValidation(event) {
+  event.preventDefault();
+
+  if ((document.getElementById('first-name').value === '') || (document.getElementById('last-name').value === '') || (document.getElementById('phone-email') === '') || (document.getElementById('new_password') === '') || (document.getElementById('birth-date') === '')) {
+    const divErrorMessage = document.getElementById('error-message');
+    const errorMessage = document.createElement('p');
+    errorMessage.innerHTML = 'Campos inválidos';
+    divErrorMessage.appendChild(errorMessage);
+  }
+}
+
+
 window.onload = function () {
+  const buttonLogin = document.getElementById('button-login');
   buttonLogin.addEventListener('click', buttonAlert);
+
+  const clickPersonalizado = document.getElementsByClassName('gender')[2];
   clickPersonalizado.addEventListener('click', newOption);
+
+  const buttonRegister = document.getElementById('facebook-register');
+  buttonRegister.addEventListener('click', dataValidation);
 };
