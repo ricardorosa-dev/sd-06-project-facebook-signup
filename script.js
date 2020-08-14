eaef89a4584d388a9e3bd5e98ed78d43eececd94
Paulo Frazão <paulo4187@gmail.com>
2020-08-14 12:33:52 -0300

    validação de todos os campos

--- script.js
+++ script.js
@@ -1,21 +1,73 @@
 const buttonLogin = document.querySelector('#button-login');
 const inputUser = document.querySelector('#user-email-phone');
+const inputPass = document.querySelector('#user-password');
+const inputName = document.querySelector('#name');
+const inputSob = document.querySelector('#sob');
+const inputMail = document.querySelector('#cel-email');
+const inputNewPass = document.querySelector('#new-pass');
 const inputPerson = document.querySelector('#personalizado');
 const divGenPerson = document.querySelector('#gen-person');
+const buttonRegister = document.querySelector('#facebook-register');
+const inputMasc = document.querySelector('#masculino');
+const inputFem = document.querySelector('#feminino');
+const inputDtns = document.querySelector('#date-nsc');
 
-buttonLogin.addEventListener('click', function () {
-  alert(inputUser.value);
-});
+function exibeUserAlert() {
+  buttonLogin.addEventListener('click', function () {
+    alert(inputUser.value);
+  });
+}
+exibeUserAlert();
 
-inputPerson.addEventListener('click', function () {
-  const elemFather = document.querySelector('#gen-person > #custom');
-  if (!elemFather) {
-    const inputGenPerson = document.createElement('input');
-    inputGenPerson.type = 'text';
-    inputGenPerson.name = 'gender-custom';
-    inputGenPerson.id = 'custom';
-    inputGenPerson.placeholder = 'Gênero (opcional)';
-    inputGenPerson.style.width = '295px';
-    divGenPerson.appendChild(inputGenPerson);
-  }
-});
+function createInputGenPerson() {
+  inputPerson.addEventListener('click', function () {
+    const elemFather = document.querySelector('#gen-person > #custom');
+    if (!elemFather) {
+      const inputGenPerson = document.createElement('input');
+      inputGenPerson.type = 'text';
+      inputGenPerson.name = 'gender-custom';
+      inputGenPerson.id = 'custom';
+      inputGenPerson.placeholder = 'Gênero (opcional)';
+      inputGenPerson.style.width = '295px';
+      divGenPerson.appendChild(inputGenPerson);
+    }
+  });
+}
+createInputGenPerson();
+
+function validateInputs() {
+  buttonRegister.addEventListener('click', function () {
+    if ((inputName.value === '' || inputSob.value === '' || inputMail.value === ''
+      || inputDtns.value === '' || inputNewPass.value === '')
+      || (inputMasc.checked === false && inputFem.checked === false && inputPerson.checked === false)) {
+      alert('Campos inválidos');
+    }
+    if (inputPerson.checked === true) {
+      const inputNewGenero = document.querySelector('#custom');
+      if (inputNewGenero.value === '') {
+        alert('Campos inválidos');
+      }
+    }
+  });
+}
+validateInputs();
+
+function inputFemRemoveGenPerson() {
+  inputFem.addEventListener('click', function () {
+    const elemFather = document.querySelector('#gen-person > #custom');
+    if (elemFather) {
+      divGenPerson.lastChild.remove();
+    }
+  });
+}
+inputFemRemoveGenPerson();
+
+function inputMascRemoveGenPerson() {
+  inputMasc.addEventListener('click', function () {
+    const elemFather = document.querySelector('#gen-person > #custom');
+    if (elemFather) {
+      divGenPerson.lastChild.remove();
+    }
+  });
+}
+inputMascRemoveGenPerson();
