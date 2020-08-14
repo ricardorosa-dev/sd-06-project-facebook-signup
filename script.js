5bbcb287fd74c57676bef9e2ff8b824d0f865ab0
CristianoNunes <cristianonunes99@gmail.com>
2020-08-14 09:28:58 -0300

    Corrigindo Fixbugs codeclimate

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function btnEntrarAlert(){
-    const btnEntrar = document.getElementById('button-login');
-    btnEntrar.addEventListener('click', () => {alert(document.querySelector('#user-email-phone').value)});
+function btnEntrarAlert() {
+  const btnEntrar = document.getElementById('button-login');
+  btnEntrar.addEventListener('click', () => { alert(document.querySelector('#user-email-phone').value); });
 }
-window.onload = function(){
-    btnEntrarAlert();
-}
+window.onload = function () {
+  btnEntrarAlert();
+};
