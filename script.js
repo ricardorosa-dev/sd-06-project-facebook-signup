2407200ad488e6af71995c089a3d8f76c3b27010
Marxneves <marxkhristie@gmail.com>
2020-08-13 23:59:50 -0300

    Verificando o ultimo requisito

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ document.querySelector('#button-login').addEventListener('click', () => {
   const EMAILORPHONE = document.querySelector('#user-email-phone').value;
   alert(EMAILORPHONE);
 });
-document.querySelector('#facebook-register').addEventListener('click', () => {
+
+document.querySelector('#facebook-register').addEventListener('click', (event) => {
+  event.preventDefault();
   let invalidForm = false;
   document.querySelectorAll('.sign-in').forEach((element) => {
     if (element.value === '') {
@@ -30,6 +32,22 @@ document.querySelector('#facebook-register').addEventListener('click', () => {
     if (!document.querySelector('.feedback-bad-action')) {
       addFeedback();
     }
+  } else {
+    const inputs = document.querySelectorAll('.right-content input');
+    let radio = '';
+    inputs.forEach((element) => {
+      if (element.type === 'radio' && element.checked) {
+        radio = element.value;
+      }
+    });
+    const rightContent = document.querySelector('.right-content');
+    document.querySelector('.main-content').removeChild(rightContent);
+    const div = document.createElement('div');
+    div.className = 'right-content';
+    const filho = document.createElement('div');
+    filho.innerHTML = `<p>Olá, ${inputs[0].value} ${inputs[1].value}</p> <p>${inputs[2].value}</p <p>${inputs[4].value}</p <p>${radio}</p`;
+    div.appendChild(filho);
+    document.querySelector('.main-content').appendChild(div);
   }
 });
 document.querySelector('#personalizado').addEventListener('click', () => {
