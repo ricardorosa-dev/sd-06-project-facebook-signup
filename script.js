aa984fcd39663ca4c70e2b202e67867574573b68
FPhilipi <fabiphilipi@gmail.com>
2020-08-17 19:20:44 -0300

    Revisão código

--- script.js
+++ script.js
@@ -1,219 +1,66 @@
-function generateErrorElement() {
-  let errorElement = document.querySelector('.error');
-  const parentElement = document.querySelector('#signUp');
-  if (errorElement == null) {
-    errorElement = document.createElement('div');
-    errorElement.classList = 'error';
-    errorElement.innerText = 'Campos inválidos';
-    parentElement.append(errorElement);
-  }
-}
+function buttonAlert() {
+    const buttonLogin = document.querySelector('#button-login');
+    const userEmailPhone = document.querySelector('#user-email-phone');
 
-function removeErrorElement() {
-  const errorElement = document.querySelector('.error');
-  if (errorElement.length > 0) {
-    errorElement.parentElement.removeChild(errorElement);
-  }
-}
-
-function insertTitleContent(map, rightContainer) {
-  const newElement = document.createElement('h1');
-  newElement.classList = 'new-title';
-  newElement.innerText = `Olá, ${map.get('firstname')} ${map.get('lastname')}`;
-  rightContainer.appendChild(newElement);
-}
-
-function insertParagraphContent(map, rightContainer) {
-  map.forEach(function (value, key) {
-    const newParagraph = document.createElement('p');
-    if (key !== 'password' && key !== 'lastname' && key !== 'firstname') {
-      newParagraph.classList = 'new-paragraph quick-easy';
-      newParagraph.innerText = `${value}`;
-      rightContainer.appendChild(newParagraph);
-    }
-  });
-}
-
-function createDataContainer(map) {
-  const rightContainer = document.createElement('article');
-  const mainContainer = document.getElementsByClassName('main-content')[0];
-  rightContainer.classList = 'right-content';
-  mainContainer.appendChild(rightContainer);
-  insertTitleContent(map, rightContainer);
-  insertParagraphContent(map, rightContainer);
-}
-
-function removeDataContainer() {
-  const parentElemen = document.getElementsByClassName('main-content')[0];
-  const childElement = document.getElementById('article');
-  parentElemen.removeChild(childElement);
-}
-
-function getInputGenderData(input) {
-  const inputGenderMap = new Map();
-  if (input.checked) {
-    inputGenderMap.set(input.name, input.value);
-  }
-  return inputGenderMap;
-}
-
-function getData() {
-  const inputs = document.querySelectorAll('main input');
-  let inputsMap = new Map();
-  const dataMap = new Map();
-  for (let i = 0; i < inputs.length; i += 1) {
-    if (i <= 4) {
-      dataMap.set(inputs[i].name, inputs[i].value);
-    } else {
-      // reference  https://stackoverflow.com/questions/36959923/javascript-concat-two-map-objects //
-      inputsMap = new Map([...inputsMap].concat([...getInputGenderData(inputs[i])]));
-    }
-  }
-  inputsMap = new Map([...dataMap].concat([...inputsMap]));
-
-  return inputsMap;
-}
-
-function showData(filledElements, inputs) {
-  if (filledElements >= (inputs.length) - 2) {
-    const dataMap = getData();
-    removeErrorElement();
-    removeDataContainer();
-    createDataContainer(dataMap);
-  }
-}
-
-function setFilledState(inputs) {
-  let filledElements = 0;
-  for (let i = 0; i < inputs.length; i += 1) {
-    const currentState = inputs[i].getAttribute('filled');
-    if (currentState !== 'true') {
-      generateErrorElement();
-    } else if (!inputs[i].classList.contains('input-gender')) {
-      filledElements += 1;
-    }
-  }
-  filledElements += 1;
-  showData(filledElements, inputs);
-}
-
-function isFilled(inputs) {
-  if ((inputs.value !== '' && inputs.value !== undefined) || inputs.checked) {
-    inputs.setAttribute('filled', 'true');
-  }
-}
-
-function isFilledTextInput(inputText) {
-  isFilled(inputText);
-  if (inputText.value === '' || inputText.value === undefined) {
-    inputText.setAttribute('filled', 'false');
-  }
-}
-
-function isFilledRadioInput(inputRadio) {
-  isFilled(inputRadio);
-  if (!inputRadio.checked) {
-    inputRadio.setAttribute('filled', 'false');
-  }
-}
-
-function isFilledInputs(inputs) {
-  for (let i = 0; i < inputs.length; i += 1) {
-    if (inputs[i].type === 'text' || inputs[i].type === 'password') {
-      isFilledTextInput(inputs[i]);
-    } else if (inputs[i].type === 'radio') {
-      isFilledRadioInput(inputs[i]);
-    }
-  }
-}
-
-function validateInputs(inputs) {
-  isFilledInputs(inputs);
-  setFilledState(inputs);
-}
-
-function initAttributes(inputs) {
-  for (let i = 0; i < inputs.length; i += 1) {
-    inputs[i].setAttribute('filled', 'false');
-  }
-}
-
-function checkCustomInputExistence() {
-  const customInput = document.getElementById('custom-input');
-  if (customInput) {
-    return true;
-  }
-  return false;
-}
-
-function generateCustomInput() {
-  const existent = checkCustomInputExistence();
-  if (!existent) {
-    const newInput = document.createElement('input');
-    const buttonArea = document.querySelector('.button-area');
-    newInput.type = Text;
-    newInput.name = 'gender-custom';
-    newInput.placeholder = 'Gênero (opcional)';
-    newInput.classList = 'input-gender input';
-    newInput.id = 'custom-input';
-    newInput.style.marginBottom = '10px';
-    buttonArea.insertAdjacentElement('beforebegin', newInput);
-    newInput.addEventListener('blur', () => {
-      isFilledInputs(newInput);
+    buttonLogin.addEventListener('click', function () {
+        alert(userEmailPhone.value);
     });
+}
+
+buttonAlert();
+
+function validateFieldsMessage() {
+    const buttonRegister = document.querySelector('#register-form');
+    const validateMessage = document.createElement('div');
+    validateMessage.innerText = 'Campos inválidos';
+    validateMessage.classList.add('invalide-warning');
+    buttonRegister.appendChild(validateMessage);
+}
+
+function validateForms() {
+    const field = document.querySelectorAll('input');
+    let isValid = true;
+
+    for (let index = 0; index < field.length; index += 1) {
+        if (field[index].value === '') {
+            isValid = false;
+        }
+    }
+
+    if (!isValid) {
+        validateFieldsMessage();
+    }
+}
+
+const registerButton = document.querySelector('#facebook-register');
+registerButton.addEventListener('click', validateForms);
+
+let existeOInputExtra = false;
+const otherGenreRadio = document.querySelector('#gen-others');
+
+function generateOtherGenreInput() {
+  const otherGenreInputWrapper = document.querySelector('.other-gen-input');
+  if (otherGenreRadio.checked === true && existeOInputExtra === false) {
+    const otherGenreInput = document.createElement('input');
+    otherGenreInput.setAttribute('name', 'gender-custom');
+    otherGenreInput.setAttribute('placeholder', 'Gênero');
+
+    otherGenreInputWrapper.appendChild(otherGenreInput);
+    existeOInputExtra = true;
   }
 }
 
-function removeCustomInput() {
-  const customInput = document.querySelector('#custom-input');
-  const parentElement = customInput.parentNode;
-  parentElement.removeChild(customInput);
+otherGenreRadio.addEventListener('click', generateOtherGenreInput);
+
+function tirarOInputExtraQuandoClicarEmOutroRadio() {
+  const otherGenreInputWrapper = document.querySelector('.other-gen-input');
+  otherGenreInputWrapper.removeChild(otherGenreInputWrapper.firstElementChild);
+  existeOInputExtra = false;
 }
 
-function validateCustomInputRemoval(inputsGender) {
-  for (let i = 0; i < inputsGender.length; i += 1) {
-    inputsGender[i].addEventListener('blur', () => {
-      const customInput = document.getElementById('custom-input');
-      if (!inputsGender[2].checked && customInput) {
-        removeCustomInput();
-      }
-    });
-  }
-}
+const mRadio = document.querySelector('#gen-masc');
+const fRadio = document.querySelector('#gen-fem');
 
-function addCustomInputEvent(inputsGender) {
-  inputsGender[2].addEventListener('click', function (e) {
-    generateCustomInput(inputsGender[2], e);
-  });
-  validateCustomInputRemoval(inputsGender);
-}
+mRadio.addEventListener('click', tirarOInputExtraQuandoClicarEmOutroRadio);
+fRadio.addEventListener('click', tirarOInputExtraQuandoClicarEmOutroRadio);
 
-function inputEvents() {
-  const inputs = document.querySelectorAll('main input');
-  const inputsGender = document.querySelectorAll('.input-gender');
-  for (let i = 0; i < inputs.length; i += 1) {
-    inputs[i].addEventListener('blur', function () {
-      isFilledInputs(inputs);
-    });
-  }
-  addCustomInputEvent(inputsGender);
-}
-
-function buttonEvents() {
-  const inputs = document.querySelectorAll('main input');
-  const buttonLogin = document.getElementById('button-login');
-  const buttonConclude = document.getElementById('facebook-register');
-  buttonConclude.addEventListener('click', function (e) {
-    initAttributes(inputs);
-    validateInputs(inputs);
-    e.preventDefault();
-  });
-  buttonLogin.addEventListener('click', function () {
-    alert(document.getElementById('user-email-phone').value);
-  });
-}
-
-window.onload = () => {
-  buttonEvents();
-  inputEvents();
-};
