bc9c076f78f4c32ddbd00aa0b8c388907e54112e
diaugustdev <diaugustdev@gmail.com>
2020-09-23 17:17:44 -0300

    until step 7 with js

---
+++ script.js
@@ -0,0 +1,16 @@
+const buttonLogin = document.querySelector('#button-login');
+const userEmailPhone = document.querySelector('#user-email-phone')
+const userPassword = document.querySelector('#user-password')
+
+buttonLogin.addEventListener('click', () => {
+    if (userEmailPhone.value.length == 0 && userPassword.value.length == 0) {
+        alert("Você precisa preencher seu e-mail e senha")
+    } else if (userEmailPhone.value.length != 0 && userPassword.value.length == 0) {
+        alert("Você não preencheu sua senha")
+    } else if (userEmailPhone.value.length == 0 && userPassword.value.length != 0) {
+        alert("Você não preencheu seu e-mail")
+    } else {
+        alert(`Seu email é: ${userEmailPhone.value}
+sua senha é: ${userPassword.value}`)
+    }
+  });
